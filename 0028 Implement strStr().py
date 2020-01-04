c5de9e1829aec873d4ea6c4421d3f4a117029cad
Yuanyuan Qiu <50243732+YuanyuanQiu@users.noreply.github.com>
2020-01-04 12:45:13 +0800

    Add files via upload

---
+++ 0028 Implement strStr().py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jan  4 12:26:25 2020
+
+@author: ToxicCat
+"""
+
+def strStr(haystack: str, needle: str) -> int:
+    return haystack.find(needle)
+
+print(strStr('hello',''))
