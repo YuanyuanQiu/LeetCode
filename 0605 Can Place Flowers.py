b5bbffe63ea9d78320f8e0d5e9d74dabfca909d9
Yuanyuan Qiu <50243732+YuanyuanQiu@users.noreply.github.com>
2020-06-13 21:09:45 -0700

    Add files via upload

---
+++ 0605 Can Place Flowers.py
@@ -0,0 +1,11 @@
+def canPlaceFlowers(flowerbed, n):
+    flowerbed = [0]+ flowerbed
+    flowerbed = flowerbed+[0]
+    for i in range(1,len(flowerbed)-1):
+        if  flowerbed[i-1] == 0 and flowerbed[i] == 0 and flowerbed[i+1] == 0:
+            n = n-1
+            flowerbed[i] = 1
+    return n<=0
+
+
+print(canPlaceFlowers(flowerbed = [1,0,0,0,0,1], n = 2))
