c9271db03b5bdd2f7aa5d539ade8f41376ba0ea2
Yuanyuan Qiu <50243732+YuanyuanQiu@users.noreply.github.com>
2020-06-29 05:09:38 +0800

    Update 0001 Two Sum.py

--- 0001 Two Sum.py
+++ 0001 Two Sum.py
@@ -1,18 +1,25 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Wed Jan  1 10:01:15 2020
+# # 暴力法
+# def twoSum(nums, target):
+#     for i in range(len(nums)):
+#         if target - nums[i] in nums[i+1:]:
+#             return [i, nums[i+1:].index(target - nums[i])+i+1]
 
-@author: ToxicCat
-"""
-# target=num1+num2 --> num1=target-num2
+# # target=num1+num2 --> num1=target-num2
+# def twoSum(nums, target):
+#     hashmap={} # 定义空字典
+#     for ind,num in enumerate(nums): #enumerate函数结果(index,列表元素)
+#         hashmap[num] = ind # 遍历存储{列表元素：index}
+#     for i,num in enumerate(nums):
+#         j = hashmap.get(target - num) # 获得num1与num2
+#         if j is not None and i!=j: # num1与num2需不相同
+#             return [i,j]
 
+# 遍历列表同时查字典
 def twoSum(nums, target):
-    hashmap={} # 定义空字典
-    for ind,num in enumerate(nums): #enumerate函数结果(index,列表元素)
-        hashmap[num] = ind # 遍历存储{列表元素：index}
-    for i,num in enumerate(nums):
-        j = hashmap.get(target - num) # 获得num1与num2
-        if j is not None and i!=j: # num1与num2需不相同
-            return [i,j]
+    dct = {}
+    for i, n in enumerate(nums):
+        if target - n in dct:
+            return [dct[target - n], i]
+        dct[n] = i
 
 print(twoSum(nums=[2, 7, 11, 15],target=9))
