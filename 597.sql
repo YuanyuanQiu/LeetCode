ad2248fc9fe2bc4d368fb43b5108d379bab5a83a
Yuanyuan Qiu <50243732+YuanyuanQiu@users.noreply.github.com>
2020-08-10 13:43:45 -0500

    update

--- 597.sql
+++
@@ -1,5 +0,0 @@
-select round(
-		ifnull(
-		(select count(distinct requester_id, accepter_id) from request_accepted) /
-		(select count(distinct sender_id, send_to_id) from friend_request)
-		,0),2) as accept_rate
