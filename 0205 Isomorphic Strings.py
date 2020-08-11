acf9bbb91c1334a25c4a258970f7f10457bafb9c
Yuanyuan Qiu <50243732+YuanyuanQiu@users.noreply.github.com>
2020-08-11 14:32:57 -0500

    update

--- 0205 Isomorphic Strings.py
+++ 0205 Isomorphic Strings.py
@@ -1,7 +1,42 @@
-'''
-同构代表两个字符串中每个位置上字符在自身第一次出现的索引相同
-'''
+# 双字典
+def isIsomorphic(s, t):
+    if len(s) != len(t):
+        return False
+    
+    sdic = {}
+    for i in range(len(s)):
+        sdic[s[i]] = sdic.get(s[i],[]) + [i]
+    
+    tdic = {}
+    for j in range(len(t)):
+        tdic[t[j]] = tdic.get(t[j],[]) + [j]
+    
+    sv = list(sdic.values())
+    tv = list(tdic.values())
+    sv.sort()
+    tv.sort()
+    if sv == tv:
+        return True
+    else:
+        return False
 
+
+# 单字典 {s[i]:t[i]}
+def isIsomorphic(s, t):
+    dct = {}
+    for i in range(len(s)):
+        if s[i] not in dct:
+            if t[i] in dct.values():
+                return False
+            dct[s[i]] = t[i]
+        else:
+            if dct[s[i]] != t[i]:
+                return False
+    return True
+
+
+# 同构代表两个字符串中每个位置上字符在自身第一次出现的索引相同
+# map(function, iterable, ...)
 def isIsomorphic(s, t):
     return [*map(s.index, s)] == [*map(t.index, t)]
 
