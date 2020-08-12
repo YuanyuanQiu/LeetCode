e4aa431838fcefdabe33d28335c294793fc0de54
Yuanyuan Qiu <50243732+YuanyuanQiu@users.noreply.github.com>
2020-08-12 16:28:30 -0500

    Create 0083 Remove Duplicates from Sorted List.py

---
+++ 0083 Remove Duplicates from Sorted List.py
@@ -0,0 +1,36 @@
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+node1 = ListNode(1)
+node2 = ListNode(1)
+node3 = ListNode(2)
+
+node1.next = node2
+node2.next = node3
+
+# def printList(node):
+#     while node:
+#         print(node.val)
+#         node = node.next
+
+# printList(node1)
+
+head = node1
+
+def deleteDuplicates(head):
+    node = head
+    
+    # 不为最后一个node
+    while node and node.next:
+        if node.val == node.next.val:
+            node.next = node.next.next
+        else:
+            node = node.next
+    
+    # head是头节点，位置没变，node做了引用，用node做了链表的操作
+    return head
+
+print(deleteDuplicates(head))
