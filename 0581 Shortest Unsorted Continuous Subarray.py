fbae5be209416247605082cb685c303bdf2905bd
Yuanyuan Qiu <50243732+YuanyuanQiu@users.noreply.github.com>
2020-08-23 11:15:41 -0500

    Update 0581 Shortest Unsorted Continuous Subarray.py

--- 0581 Shortest Unsorted Continuous Subarray.py
+++ 0581 Shortest Unsorted Continuous Subarray.py
@@ -7,12 +7,43 @@
 #         if nums[i] != origin[i]:
 #             start = i
 #             break
+#     else:
+#         return 0
+    
 #     for j in range(len(nums)):
 #         if nums[::-1][j] != origin[::-1][j]:
 #             end = j
 #             break
     
 #     return len(nums)-end-start
+
+
+# def findUnsortedSubarray(nums):
+#     if len(nums) <= 1:
+#         return 0
+
+#     nums_sorted = nums.copy()
+#     nums_sorted.sort()
+
+#     l = 0
+#     r = len(nums) - 1
+
+#     while l <= r:
+#         if nums[l] == nums_sorted[l]:
+#             l += 1
+#         else:
+#             break
+#     if l == r:
+#         return len(nums)
+
+
+#     while r>l:
+#         if nums[r] == nums_sorted[r]:
+#             r -= 1
+#         else:
+#             break
+
+#     return r - l + 1
 
 
 # 获取所有当前数组与排序后数组具有不同数值的索引，最右边的索引 - 最左边的 + 1 就是结果
