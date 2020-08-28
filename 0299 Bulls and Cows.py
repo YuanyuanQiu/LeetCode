a18f80023a1add363448f064fa3a586462434da4
Yuanyuan Qiu <50243732+YuanyuanQiu@users.noreply.github.com>
2020-08-28 16:54:11 -0500

    Update 0299 Bulls and Cows.py

--- 0299 Bulls and Cows.py
+++ 0299 Bulls and Cows.py
@@ -16,5 +16,22 @@ def getHint(secret, guess):
     return str(bulls)+'A'+str(cows)+'B'
 
 print(getHint('1122','2211'))
-            
-        
+
+
+def getHint(secret, guess):
+    bulls = 0
+    cows = 0
+    
+    dic = {}
+    for i in range(len(secret)):
+        if secret[i] in guess:
+            dic[secret[i]] = dic.get(secret[i],0) + 1
+        if secret[i] == guess[i]:
+            bulls += 1
+    
+    for j in guess:
+        if dic.get(j,0) > 0:
+            cows += 1
+            dic[j] = dic.get(j,0) - 1
+    
+    return str(bulls)+'A'+str(cows-bulls)+'B'
