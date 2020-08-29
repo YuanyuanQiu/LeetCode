3039b14e299fc4766a542405d035e656eb47d532
Yuanyuan Qiu <50243732+YuanyuanQiu@users.noreply.github.com>
2020-08-29 17:53:41 -0500

    Create 1355.sql

---
+++ 1355.sql
@@ -0,0 +1,13 @@
+select activity
+from Friends
+group by activity
+having count(id) != (select count(id)
+					from Friends
+                    group by activity
+                    order by count(id) desc
+                    limit 1)
+and count(id) != (select count(id)
+					from Friends
+                    group by activity
+                    order by count(id)
+                    limit 1)
