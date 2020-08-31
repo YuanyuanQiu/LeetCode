996b38e9b451530d04a0f8c5752a9ea0d75ac134
Yuanyuan Qiu <50243732+YuanyuanQiu@users.noreply.github.com>
2020-08-31 16:22:05 -0500

    Create 1532.sql

---
+++ 1532.sql
@@ -0,0 +1,12 @@
+select c.name customer_name, c.customer_id, t.order_id, t.order_date
+from
+	(select
+		customer_id,
+        order_id,
+        order_date, 
+		dense_rank() over (partition by customer_id order by order_date desc) rnk
+    from Orders) t
+left join Customers c
+on t.customer_id = c.customer_id
+where rnk <= 3
+order by c.name, c.customer_id, t.order_date desc
