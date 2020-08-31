d8ff94b075bdf628088e30785786e551c7dbca31
Yuanyuan Qiu <50243732+YuanyuanQiu@users.noreply.github.com>
2020-08-31 11:50:50 -0500

    Update 0605 Can Place Flowers.py

--- 0605 Can Place Flowers.py
+++ 0605 Can Place Flowers.py
@@ -1,6 +1,6 @@
 def canPlaceFlowers(flowerbed, n):
-    flowerbed = [0]+ flowerbed
-    flowerbed = flowerbed+[0]
+    flowerbed = [0] + flowerbed + [0]
+
     for i in range(1,len(flowerbed)-1):
         if  flowerbed[i-1] == 0 and flowerbed[i] == 0 and flowerbed[i+1] == 0:
             n = n-1
