a51536c411c560ba1ccf2598f45b8d6cbaf116a5
Yuanyuan Qiu <50243732+YuanyuanQiu@users.noreply.github.com>
2020-09-01 15:54:06 -0500

    Update 0001 Two Sum.py

--- 0001 Two Sum.py
+++ 0001 Two Sum.py
@@ -1,19 +1,3 @@
-# # 暴力法
-# def twoSum(nums, target):
-#     for i in range(len(nums)):
-#         if target - nums[i] in nums[i+1:]:
-#             return [i, nums[i+1:].index(target - nums[i])+i+1]
-
-# # target=num1+num2 --> num1=target-num2
-# def twoSum(nums, target):
-#     hashmap={} # 定义空字典
-#     for ind,num in enumerate(nums): #enumerate函数结果(index,列表元素)
-#         hashmap[num] = ind # 遍历存储{列表元素：index}
-#     for i,num in enumerate(nums):
-#         j = hashmap.get(target - num) # 获得num1与num2
-#         if j is not None and i!=j: # num1与num2需不相同
-#             return [i,j]
-
 # 遍历列表同时查字典
 def twoSum(nums, target):
     dct = {}
