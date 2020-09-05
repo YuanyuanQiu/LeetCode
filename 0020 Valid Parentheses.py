53656b98df301365cf01b1667f3c1e667f7ac086
Yuanyuan Qiu <50243732+YuanyuanQiu@users.noreply.github.com>
2020-09-05 18:25:51 -0500

    Update 0020 Valid Parentheses.py

--- 0020 Valid Parentheses.py
+++ 0020 Valid Parentheses.py
@@ -1,5 +1,6 @@
 def isValid(s: str) -> bool:
-    '''字典key:value
+    '''
+    字典：key=左括号, value=右括号
     栈：遍历每个元素，若左放入字符串，若右则匹配字符串最后一个是否登对，是则去掉,
     否则False    
     '''    
@@ -11,14 +12,14 @@ def isValid(s: str) -> bool:
     
     dict1={'(':')','{':'}','[':']'}
     
-    ls = []
+    ls = [] 只存左括号
     for i in s:
+        # 左括号
         if i in dict1.keys():
             ls.append(i)
+        # 右括号
         else:
-            if ls == []:
-                return False
-            elif dict1[ls[-1]] != i:
+            if ls == [] or dict1[ls[-1]] != i:
                 return False
             else:
                 del ls[-1]
