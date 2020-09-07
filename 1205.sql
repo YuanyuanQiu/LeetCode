49065eb6a35a54a557a03c66b6648b69ff7f77db
Yuanyuan Qiu <50243732+YuanyuanQiu@users.noreply.github.com>
2020-09-06 20:40:05 -0500

    Update 1205.sql

--- 1205.sql
+++ 1205.sql
@@ -1,16 +1,17 @@
-select
-	month,
-	country,
-	count(case when state='approved' and tag=1 then 1 end) as approved_count,
-	sum(case when tag=1 then amount else 0 end) as approved_amount,
-	count(case when tag=0 then 1 end) as chargeback_count,
-	sum(case when tag=0 then amount else 0 end) as chargeback_amount
-from (select country,state,amount,date_format(trans_date,"%Y-%m") as month,1 as tag
-		from Transactions
-		where state='approved'
-		union all
-		select country,state,amount,date_format(c.trans_date,"%Y-%m") as month,0 as tag
-		from Transactions t
-        right join Chargebacks c
-		on c.trans_id=t.id) as a
-group by month,country;
+SELECT
+    month,
+    country,
+    COUNT(IF(tag=1, 1, NULL)) AS approved_count,
+    SUM(IF(tag=1, amount, 0)) AS approved_amount,
+    COUNT(IF(tag=0, 1, NULL)) AS chargeback_count,
+    SUM(IF(tag=0, amount, 0)) AS chargeback_amount
+FROM (SELECT country, amount, 1 AS tag, date_format(trans_date, '%Y-%m') AS month
+        FROM Transactions
+        WHERE state='approved'
+        
+        UNION ALL
+
+        SELECT country, amount, 0 AS tag, date_format(c.trans_date, '%Y-%m') AS month
+        FROM Transactions AS t RIGHT OUTER JOIN Chargebacks AS c
+        ON t.id = c.trans_id) AS temp
+GROUP BY month, country;
