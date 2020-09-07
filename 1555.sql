119707bc767105ac2802e05d8fbb4b176632b827
Yuanyuan Qiu <50243732+YuanyuanQiu@users.noreply.github.com>
2020-09-06 21:39:46 -0500

    Update 1555.sql

--- 1555.sql
+++ 1555.sql
@@ -1,8 +1,8 @@
 select
 	user_id,
     user_name,
-    round(avg(credit) + ifnull(sum(if(user_id = paid_to, 1, -1) * amount),0),0) credit,
-    if(avg(credit) + ifnull(sum(if(user_id = paid_to, 1, -1) * amount),0) < 0, 'Yes', 'No') credit_limit_breached
+    credit + ifnull(sum(if(user_id = paid_to, 1, -1) * amount),0) credit,
+    if(credit + ifnull(sum(if(user_id = paid_to, 1, -1) * amount),0) < 0, 'Yes', 'No') credit_limit_breached
 from Users
 left join Transactions
 on user_id = paid_by or user_id = paid_to
