656ea60efd3bbbbeb02b2164e4998f10b5949e4e
Yuanyuan Qiu <50243732+YuanyuanQiu@users.noreply.github.com>
2020-09-07 16:09:34 -0500

    update

--- 0028 Implement strStr().py
+++ 0028 Implement strStr().py
@@ -1,12 +1,14 @@
-# def strStr(self, haystack: str, needle: str) -> int:
-#     if needle == '':
-#         return 0
-#     ls = haystack.split(needle)
-#     if len(ls) == 1:
-#         return -1
-#     else:
-#         return len(ls[0])
+# Split
+def strStr(self, haystack: str, needle: str) -> int:
+    if needle == '':
+        return 0
+    ls = haystack.split(needle)
+    if len(ls) == 1:
+        return -1
+    else:
+        return len(ls[0])
 
+# Find
 def strStr(haystack: str, needle: str) -> int:
     return haystack.find(needle)
 
