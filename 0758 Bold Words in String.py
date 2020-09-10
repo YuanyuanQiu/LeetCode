9e8c32050b7d6271f42a1ed32e9d04073094f4ea
Yuanyuan Qiu <50243732+YuanyuanQiu@users.noreply.github.com>
2020-09-10 10:44:44 -0500

    Create 0758 Bold Words in String.py

---
+++ 0758 Bold Words in String.py
@@ -0,0 +1,35 @@
+def boldWords(words, S):
+    b = [0]*(len(S)+1)
+    
+    # 标注所有开头（1）和结尾（-1）
+    for w in words:
+        begin = 0
+        while begin != -1:
+            begin = S.find(w,begin)
+            if begin != -1:
+                # 开头
+                b[begin] += 1
+                # 结尾
+                b[begin + len(w)] -= 1
+                begin += 1
+    # print(b)
+    
+    rs = ''
+    sc = 0
+    for i,e in enumerate(b):
+        if sc == 0 and e > 0:
+            rs += '<b>'
+        elif sc > 0 and sc + e == 0:
+            rs += '</b>'
+        
+        if i < len(S):
+            rs += S[i]
+        sc += e
+        
+        # print(rs,sc)
+    return rs
+
+
+words = ["ab", "bc"]
+S = "aabcd"
+print(boldWords(words, S))
