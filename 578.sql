9caa13427051f9a81201a288f4d90b3abf4ed729
Yuanyuan Qiu <50243732+YuanyuanQiu@users.noreply.github.com>
2020-09-10 00:18:36 -0500

    Update 578.sql

--- 578.sql
+++ 578.sql
@@ -1,9 +1,5 @@
-SELECT question_id as survey_log
-FROM (SELECT
-		question_id,
-		SUM(case when action = "answer" THEN 1 ELSE 0 END) as num_answers,
-		SUM(case when action = "show" THEN 1 ELSE 0 END) as num_shows
-	FROM survey_log
-	GROUP BY question_id) as tbl
-ORDER BY (num_answers / num_shows) DESC
-LIMIT 1;
+SELECT question_id survey_log FROM survey_log 
+WHERE answer_id IS NOT NULL
+GROUP BY question_id
+ORDER BY COUNT(answer_id) DESC
+LIMIT 1
