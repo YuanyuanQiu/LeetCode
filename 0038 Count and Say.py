2aa0392ac44c320dd67ba58d42244ab4e36944ef
Yuanyuan Qiu <50243732+YuanyuanQiu@users.noreply.github.com>
2020-09-11 22:24:44 -0500

    update

--- 0038 Count and Say.py
+++ 0038 Count and Say.py
@@ -1,29 +1,19 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Sun Jan  5 21:28:21 2020
+def countAndSay(n):
+    res = ["1",]  # 当前输出外观res数列
+    r = 1
+    while r < n: # 一直算到要求的第n行为止
+        r += 1
+        p, q = 0, 1 # p初始化为0，q初始化为1，遍历当前输出外观res以获得下一输出；
+        tmp = []    # 下一行输出外观数列
+        while q <= len(res):
+            if q == len(res): # 特殊边界位置的处理：当q == len(res)刚好溢出时
+                tmp.extend([str(q-p), res[p]]) 
+            elif res[p] != res[q]:
+                tmp.extend([str(q-p), res[p]])
+                p = q  # p指向下一个不同的数字即让p=q，q重复上述2.
+            q += 1     
+        res =  tmp   # 节省空间，利用上一行计算下一行，故直接用计算出的当前行替换上一行
+    return "".join(res)
 
-@author: ToxicCat
-"""
-
-'''
-递归
-'''
-
-def countAndSay(n: int) -> str:
-    if(n == 1): return '1'
-    num = countAndSay(n-1)+"*" #递归，直到最小数字1
-    #print(num)
-    temp = num
-    count = 1 #计数器
-    strBulider = '' #字符串，记录最终结果
-    #print(len(temp))
-    for i in range(len(temp)-1): #对除了最后一位*进行遍历
-        if  temp[i] == temp[i+1]: #假设该位与后一位相同
-                count += 1 #计数器加1
-        else: #若该位与后一位不同，count复位为1
-            strBulider +=  str(count) + temp[i] #count个i
-            count = 1
-    return strBulider
 
 print(countAndSay(5))
-        
--- 1010 Pairs of Songs With Total Durations Divisible by 60.py
+++ 1010 Pairs of Songs With Total Durations Divisible by 60.py
@@ -4,15 +4,22 @@ def numPairsDivisibleBy60(time):
     for i,t in enumerate(time):
         r = t % 60
         dic[r] = dic.get(r,0) + 1
+    
+    '''
+    拿到所有余数后，其范围是 0 到 59。假设余数为 1 的有 10 个，余数为 59 的有 5 个，
+    那么我们可以算出它们可以生成 10*5 = 50 对有效结果；但这里特殊的是余数为 0 和 30 
+    的，假设余数为 30 的数字有 10 个，那么产生的不重复有效结果为 10 * 9/2 = 45 对。
 
+    '''
     count = 0
     # 遍历 0 到 30，通过 60-i 便可拿到整除 60 后所有余数可能
     for i in range(31):
-    	# 如果余数为 0  或 30，单独处理
+    	# 如果余数为 0或 30，单独处理
         if i in [0,30]:
             num = dic.get(i)
             if num and num>1:
-                count+=num*(num-1)//2 # (num-1)+(num-2)+...+1
+                count += num*(num-1)//2 # (num-1)+(num-2)+...+1
+        
         # 正常组合为 60 的余数组，取到互配的余数个数，计算结果
         else:
             num1 = dic.get(i)
