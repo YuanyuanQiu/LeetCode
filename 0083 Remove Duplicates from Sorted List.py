90daa9697fb359bfbc37df6e3e2226ce90427db8
Yuanyuan Qiu <50243732+YuanyuanQiu@users.noreply.github.com>
2020-09-14 15:48:05 -0500

    Update 0083 Remove Duplicates from Sorted List.py

--- 0083 Remove Duplicates from Sorted List.py
+++ 0083 Remove Duplicates from Sorted List.py
@@ -23,7 +23,7 @@ head = node1
 def deleteDuplicates(head):
     node = head
     
-    # 不为最后一个node
+    # 不为空链表和最后一个node
     while node and node.next:
         if node.val == node.next.val:
             node.next = node.next.next
@@ -31,6 +31,21 @@ def deleteDuplicates(head):
             node = node.next
     
     # head是头节点，位置没变，node做了引用，用node做了链表的操作
+    return head
+
+def deleteDuplicates(head):
+    if not (head and head.next):
+        return head
+    i = head
+    j = head.next
+    while j:
+		# 如果i不等于j，则i前进一位，然后将j的值赋给i
+        if i.val!=j.val:
+            i = i.next
+            i.val = j.val # 将i的下一位赋值与i不同的j值
+        # 不管i是否等于j，j每次都前进一位
+        j = j.next
+    i.next = None
     return head
 
 print(deleteDuplicates(head))
