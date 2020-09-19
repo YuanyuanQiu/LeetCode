b2a87aaffb59ee8a0c01887fa38d9ed03492a6d5
Yuanyuan Qiu <50243732+YuanyuanQiu@users.noreply.github.com>
2020-09-19 16:44:11 -0500

    Update 0119 Pascal's Triangle II.py

--- 0119 Pascal's Triangle II.py
+++ 0119 Pascal's Triangle II.py
@@ -1,19 +1,12 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Wed Jan  8 11:21:23 2020
-
-@author: ToxicCat
-"""
-
 def getRow(rowIndex: int):
     if rowIndex == 0:
         return [1]
     elif rowIndex == 1:
         result = [1,1]
     else:
-        i=1
+        i = 2
         begin = [1,1]
-        while i <=rowIndex-1:
+        while i <= rowIndex:
             result = []
             for j in range(len(begin)-1):
                 result.append(begin[j]+begin[j+1])
