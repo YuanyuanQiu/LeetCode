6436fa502e2c35fca15497e5913633440f72d486
Yuanyuan Qiu <50243732+YuanyuanQiu@users.noreply.github.com>
2020-09-18 22:39:07 -0500

    Update 0509 Fibonacci Number.py

--- 0509 Fibonacci Number.py
+++ 0509 Fibonacci Number.py
@@ -1,6 +1,20 @@
+# 递归
 def fib(N):
     if N <= 1:
         return N
+    
     return fib(N-1)+fib(N-2)
+
+
+# 迭代
+def fib():
+    if N <= 1:
+        return N
+    
+    a, b = 0, 1
+    for _ in range(N - 1):
+        a, b = b, a + b
+    return b
+
 
 print(fib(3))
