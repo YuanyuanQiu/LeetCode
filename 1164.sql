5074dca6591acd965e55be2455ed5e2bdbfcb876
Yuanyuan Qiu <50243732+YuanyuanQiu@users.noreply.github.com>
2020-09-19 15:50:37 -0500

    Update 1164.sql

--- 1164.sql
+++ 1164.sql
@@ -14,3 +14,20 @@ FROM
 	 WHERE product_id NOT IN (SELECT product_id FROM Products WHERE change_date <= '2019-08-16')
 	) tmp
 ORDER BY price DESC
+
+# method 2
+select
+    t1.product_id,
+    (case
+        when change_date is null then 10
+        else new_price
+    end) as price
+from (select distinct product_id
+        from Products) t1
+left join (select product_id, new_price, change_date
+            from Products
+            where (product_id, change_date) in (select product_id, max(change_date)
+                                                from Products
+                                                where change_date <= '2019-08-16'
+                                                group by product_id)) t2
+on t1. product_id = t2.product_id
