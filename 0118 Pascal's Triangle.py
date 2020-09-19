f583445eb1cb4fb034c8f5ffa834245b946507f9
Yuanyuan Qiu <50243732+YuanyuanQiu@users.noreply.github.com>
2020-09-18 22:21:08 -0500

    Update 0118 Pascal's Triangle.py

--- 0118 Pascal's Triangle.py
+++ 0118 Pascal's Triangle.py
@@ -1,33 +1,44 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Wed Jan  8 10:42:16 2020
+# '''
+# numRows = 0/1/2特例
+# numRows > 3：首尾加1，第二位开始遍历上一层相加
+# '''
 
-@author: ToxicCat
-"""
+# def generate(numRows: int):
+#     if numRows == 0:
+#         return []
+#     elif numRows == 1:
+#         result = [[1]]
+#     elif numRows == 2:
+#         result = [[1],[1,1]]
+#     else:
+#         result = [[1],[1,1]]
+#         i=1
+#         while i <=numRows-2:
+#             result.append([])
+#             for j in range(len(result[-2])-1):
+#                 result[-1].append(result[-2][j]+result[-2][j+1])
+#             result[-1].insert(0,1)
+#             result[-1].append(1)
+#             i+=1
+#     return result
 
-'''
-numRows = 0/1/2特例
-numRows > 3：首尾加1，第二位开始遍历上一层相加
-'''
 
-def generate(numRows: int):
-    if numRows == 0:
-        return []
-    elif numRows == 1:
-        result = [[1]]
-    elif numRows == 2:
-        result = [[1],[1,1]]
-    else:
-        result = [[1],[1,1]]
-        i=1
-        while i <=numRows-2:
-            result.append([])
-            for j in range(len(result[-2])-1):
-                result[-1].append(result[-2][j]+result[-2][j+1])
-            result[-1].insert(0,1)
-            result[-1].append(1)
-            i+=1
-    return result
+def generate(num_rows):
+    triangle = []
 
-print(generate(5))
+    for row_num in range(num_rows):
+        # The first and last row elements are always 1.
+        row = [None for _ in range(row_num+1)]
+        row[0], row[-1] = 1, 1
+
+        # Each triangle element is equal to the sum of the elements
+        # above-and-to-the-left and above-and-to-the-right.
+        for j in range(1, len(row)-1):
+            row[j] = triangle[row_num-1][j-1] + triangle[row_num-1][j]
+
+        triangle.append(row)
+
+    return triangle
+
+print(generate(0))
     
