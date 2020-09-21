52ef0c0e444966118eb1083d59614eba1990dabe
Yuanyuan Qiu <50243732+YuanyuanQiu@users.noreply.github.com>
2020-09-21 09:43:50 -0500

    Update 1174.sql

--- 1174.sql
+++ 1174.sql
@@ -1,8 +1,10 @@
-select round(sum(order_date = customer_pref_delivery_date)
-            / count(*) * 100,2) as immediate_percentage
-from
-	(select *
+select round(avg(immediate) * 100,2) as immediate_percentage
+from (select
+        (case
+            when order_date = customer_pref_delivery_date then 1 else 0
+        end)as immediate
     from Delivery
-    where order_date = min(order_date)
-    group by customer_id
+    where (customer_id, order_date) in (select customer_id, min(order_date)
+                                        from Delivery
+                                        group by customer_id)
     ) t
