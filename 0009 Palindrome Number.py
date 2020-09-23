4a34fb0f7ab033c14595c6d0294ab68b74e3b10a
Yuanyuan Qiu <50243732+YuanyuanQiu@users.noreply.github.com>
2020-09-23 15:33:09 -0500

    Update 0009 Palindrome Number.py

--- 0009 Palindrome Number.py
+++ 0009 Palindrome Number.py
@@ -1,28 +1,12 @@
-# def isPalindrome(x):
-#         if x<0: #负数为False
-#             return False
-#         else:
-#             m,n = x,0 #m 表示原数字，n表示反转后数字
-#             while m:
-#                 n = n*10 + m%10 #m取最后一位数字，逐步前推
-#                 m = m//10 #m去除最后一位数字（//：返回整数部分
-#                 # print(m,n)
-                
-#             if x == n:
-#                 return True
-#             else:
-#                 return False
-
-
 def isPalindrome(x):
-    lst = list(str(x))
-    print(lst)
-    L, R = 0, len(lst)-1
-    while L <= R:
-        if lst[L] != lst[R]:
-            return  False
-        L += 1
-        R -= 1
-    return True
-    
+        if x<0: #负数为False
+            return False
+
+        m,n = x,0 #m 表示原数字，n表示反转后数字
+        while m:
+            n = n*10 + m%10 #m取最后一位数字，逐步前推
+            m = m//10 #m去除最后一位数字（//：返回整数部分
+                
+        return x == n
+
 print(isPalindrome(121))
