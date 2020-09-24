180f8eb8287b0562d8ac9c68375bee5d334e38d0
Yuanyuan Qiu <50243732+YuanyuanQiu@users.noreply.github.com>
2020-09-24 15:48:25 -0500

    Update 1321.sql

--- 1321.sql
+++ 1321.sql
@@ -1,15 +1,16 @@
 select
-	visit_on,
+    c1.visited_on,
     sum(c2.amount) as amount,
     round(sum(c2.amount)/7,2) as average_amount
-from 
-	(select customer_id,name,visited_on,sum(amount) amount
-    from Customer
+from
+    (select visited_on, sum(amount) as amount
+    From Customer
     group by visited_on) c1,
-	(select customer_id,name,visited_on,sum(amount) amount
-    from Customer
+    (select visited_on, sum(amount) as amount
+    From Customer
     group by visited_on) c2
-where datediff(c1.visit_on, c2.visit_on) <= 6
-	and datediff(c1.visit_on, c2.visit_on) >= 0
-group by c1.visit_on
-order by c1.visit_on
+where datediff(c1.visited_on, c2.visited_on) <= 6
+    and datediff(c1.visited_on, c2.visited_on) >= 0
+group by c1.visited_on
+having count(c2.visited_on) = 7
+order by c1.visited_on
