8b7a4720ca8d366bcf9143aed1e64b6c7a908d74
Yuanyuan Qiu <50243732+YuanyuanQiu@users.noreply.github.com>
2020-09-24 09:23:16 -0500

    Update 0035 Search Insert Position.py

--- 0035 Search Insert Position.py
+++ 0035 Search Insert Position.py
@@ -1,10 +1,3 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Sat Jan  4 12:47:23 2020
-
-@author: ToxicCat
-"""
-
 # def searchInsert(nums, target):
 #     if target in nums:
 #         result = nums.index(target)
@@ -19,19 +12,22 @@ Created on Sat Jan  4 12:47:23 2020
 #                     result = i+1
 #     return result
 
-# Method 2: 二分法
+# 二分法
 def searchInsert(nums, target):
     size = len(nums)
     if size == 0:
         return 0
+    
     left = 0
-    right = size
+    right = size # 因为有可能数组的最后一个元素的位置的下一个是我们要找的
+    
     while left < right:
         mid = left + (right - left) // 2 #取中间数
         if nums[mid] < target:
-            left = mid + 1 #若中间数小于target，中间数右移一位
+            left = mid + 1 # +1避免区间只有2个数时left=mid，进入死循环
         else:
             right = mid #若中间数大于target，中间数为右边界
+    
     return left
 
 print(searchInsert([1,3,5,6],2))
