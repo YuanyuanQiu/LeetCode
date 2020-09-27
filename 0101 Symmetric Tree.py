c9dd067ddc68981679374335f7ad348ecc36a24c
Yuanyuan Qiu <50243732+YuanyuanQiu@users.noreply.github.com>
2020-09-27 12:04:59 -0500

    Update 0101 Symmetric Tree.py

--- 0101 Symmetric Tree.py
+++ 0101 Symmetric Tree.py
@@ -26,11 +26,11 @@ def isSymmetric(self, root):
 		# 递归的终止条件是两个节点都为空
 		# 或者两个节点中有一个为空
 		# 或者两个节点的值不相等
-		if not (left or right):
+		if not left and not right:
 			return True
-		if not (left and right):
+		if not left or not right:
 			return False
-		if left.val!=right.val:
+		if left.val != right.val:
 			return False
 		return dfs(left.left,right.right) and dfs(left.right,right.left)
 		# 用递归函数，比较左节点，右节点
