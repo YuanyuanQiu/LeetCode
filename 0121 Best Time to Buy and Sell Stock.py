4812aab6c890e24b1fbb1de83c03df909df57aa9
Yuanyuan Qiu <50243732+YuanyuanQiu@users.noreply.github.com>
2020-10-02 11:16:43 -0500

    Update 0121 Best Time to Buy and Sell Stock.py

--- 0121 Best Time to Buy and Sell Stock.py
+++ 0121 Best Time to Buy and Sell Stock.py
@@ -1,13 +1,31 @@
+# 动态规划
 def maxProfit(prices):
-    # 无穷小
-    minprice = float('inf')
-    # 无穷大
+    n = len(prices)
+    # 边界条件
+    if n <= 1:
+        return 0 
+    
+    # 初始条件dp表示profit list
+    dp = [0] * n
+    minprice = prices[0] 
+
+    for i in range(1, n):
+        minprice = min(minprice, prices[i])
+        # 状态转移方程
+        dp[i] = max(dp[i - 1], prices[i] - minprice)
+
+    return dp[-1]
+
+# 动态规划优化版（一次遍历）
+def maxProfit(prices):
+    # 初始条件dp表示profit
+    minprice = float('inf') # 无穷大
     maxprofit = 0
     
     for price in prices:
         # 遍历过程中取得目前为止最小值
         minprice = min(minprice, price)
-        # 遍历过程中取得目前为止profit最大值
+        # 状态转移方程
         maxprofit = max(maxprofit, price - minprice)
     
     return maxprofit
