230ff0f68b23b939ba285dbb8bc43b5d871ff175
Yuanyuan Qiu <50243732+YuanyuanQiu@users.noreply.github.com>
2020-10-02 10:48:29 -0500

    Update 0121 Best Time to Buy and Sell Stock.py

--- 0121 Best Time to Buy and Sell Stock.py
+++ 0121 Best Time to Buy and Sell Stock.py
@@ -1,10 +1,15 @@
 def maxProfit(prices):
+    # 无穷小
     minprice = float('inf')
+    # 无穷大
     maxprofit = 0
     
     for price in prices:
+        # 遍历过程中取得目前为止最小值
         minprice = min(minprice, price)
+        # 遍历过程中取得目前为止profit最大值
         maxprofit = max(maxprofit, price - minprice)
+    
     return maxprofit
 
 print(maxProfit([7,1,5,3,6,4]))
