7578fa39116ff19ca9f515c3f9220036a118afa5
Yuanyuan Qiu <50243732+YuanyuanQiu@users.noreply.github.com>
2020-10-06 15:26:05 -0500

    Update 0171 Excel Sheet Column Number.py

--- 0171 Excel Sheet Column Number.py
+++ 0171 Excel Sheet Column Number.py
@@ -1,19 +1,11 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Wed Feb 12 08:14:57 2020
-
-@author: ToxicCat
-"""
-
 def titleToNumber(s):
-    origin=ord('A')
-    s=s[::-1]
-    col_num=0
+    origin = ord('A')
+    s = s[::-1]
+    ans = 0
     for i in range(len(s)):
-        gap=ord(s[i])-origin
-        col_num+=pow(26,i)*(1+gap)
-        # print('col_num:',col_num)
-    return col_num
+        gap = ord(s[i]) - origin
+        ans += pow(26,i)*(1 + gap)
+    return ans
 
 print(titleToNumber('AA'))
         
