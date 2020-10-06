936073faae1a3b07cc9b9b1abbb77190886a9c0e
Yuanyuan Qiu <50243732+YuanyuanQiu@users.noreply.github.com>
2020-10-06 15:58:26 -0500

    Update 0189 Rotate Array.py

--- 0189 Rotate Array.py
+++ 0189 Rotate Array.py
@@ -1,22 +1,7 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Tue Feb 25 03:15:02 2020
-
-@author: ToxicCat
-"""
-
-# def rotate(nums, k):
-#     n=len(nums)
-#     if k>0:
-#         for i in range(k):
-#             nums.insert(0,nums[-1])
-#             del nums[-1]
-#     return nums
-    
 def rotate(nums, k):
-    nums=nums[::-1]
-    m=k%len(nums)
-    nums=nums[:m][::-1]+nums[m:][::-1]
+    n = len(nums)
+    k %= n
+    nums[:] = nums[n-k:] + nums[:n-k]
     return nums
 
 print(rotate([1,3,5,7], 3))
