3961977ac85dd30b44ea0e43a7892ec37cb62c31
Yuanyuan Qiu <50243732+YuanyuanQiu@users.noreply.github.com>
2020-10-18 17:17:18 -0500

    Update 0206 Reverse Linked List.py

--- 0206 Reverse Linked List.py
+++ 0206 Reverse Linked List.py
@@ -6,11 +6,11 @@
 
 # 双指针
 def reverseList(self, head: ListNode) -> ListNode:
-    pre = None # 当前节点的前驱节点
-    cur = head # 当前遍历到的节点
+    pre = None # 前驱节点
+    cur = head # 当前节点
     while cur:
         temp = cur.next   # 先把原来cur.next位置存起来
-        cur.next = pre
-        pre = cur
-        cur = temp
+        cur.next = pre # 反转
+        pre = cur # 前驱节点指针后移一位
+        cur = temp # 当前节点指针后移一位
     return pre
