11ff65dca68a81fbe113cdb68917bdef7666c5d8
Yuanyuan Qiu <50243732+YuanyuanQiu@users.noreply.github.com>
2020-10-24 21:44:24 -0500

    Update 0231 Power of Two.py

--- 0231 Power of Two.py
+++ 0231 Power of Two.py
@@ -1,9 +1,10 @@
 def isPowerOfTwo(n):
     if n == 0:
         return False
-    result = True
-    while n%2 == 0:
+
+    while n % 2 == 0: # while remainder = 0
         n /= 2
+
     return n == 1
 
 print(isPowerOfTwo(0))
