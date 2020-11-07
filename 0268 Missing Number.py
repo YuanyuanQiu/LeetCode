3830791262b69315f6faed8c59be4523310f84cb
Yuanyuan Qiu <50243732+YuanyuanQiu@users.noreply.github.com>
2020-11-07 10:06:31 -0600

    Update 0268 Missing Number.py

--- 0268 Missing Number.py
+++ 0268 Missing Number.py
@@ -1,33 +1,15 @@
-# def missingNumber(nums):
-#     n = max(nums)
-#     nums_all=list(range(n+1))
-#     for i in nums:
-#         nums_all.remove(i)
-#     if nums_all == []:
-#         return n+1
-#     else:
-#         return nums_all[0]
-
-
-# def missingNumber(nums):
-#     nums.append('Missing')
-
-#     l = 0
-#     while l < len(nums) - 1:
-#         if nums[l] != 'Missing' and nums[l] != l:
-#             temp = nums[l]
-#             nums[l], nums[temp] =  nums[temp], nums[l]
-#         else:
-#             l += 1
-
-#     return nums.index('Missing')
-
-
 def missingNumber(nums):
     num_set = set(nums)
     n = len(nums) + 1
     for number in range(n):
         if number not in num_set:
             return number
+
+# 对一个数进行两次完全相同的异或运算会得到原来的数
+def missingNumber(nums):
+    missing = len(nums)
+    for i, num in enumerate(nums):
+        missing ^= i ^ num
+    return missing
 
 print(missingNumber([3,0,1]))
