e699074577ec675831629bb0fbc402803325daa7
Yuanyuan Qiu <50243732+YuanyuanQiu@users.noreply.github.com>
2020-12-02 11:58:08 -0600

    Update 0547 Friend Circles.py

--- 0547 Friend Circles.py
+++ 0547 Friend Circles.py
@@ -1,3 +1,4 @@
+# DFS
 class Solution:
     def findCircleNum(self, M: List[List[int]]) -> int:
         n = len(M)
@@ -17,3 +18,25 @@ class Solution:
                 dfs(i)
 
         return count
+
+# nion-find algorithm
+class Solution:
+    def findCircleNum(self, M) -> int:
+        father = [i for i in range(len(M))]
+
+        def find(a):
+            if father[a] != a:
+                father[a] = find(father[a])
+            return father[a]
+
+        def union(a, b):
+            father[find(b)] = find(a)
+            return find(b)
+
+        for a in range(len(M)):
+            for b in range(a):
+                if M[a][b]:
+                    union(a, b)
+        for i in range(len(M)):
+            find(i)
+        return len(set(father))
