b5f120d26a9ea3a3dd8149c4d2e89d028a9e7964
Yuanyuan Qiu <50243732+YuanyuanQiu@users.noreply.github.com>
2020-12-04 10:31:29 -0600

    Update 0027 Remove Element.py

--- 0027 Remove Element.py
+++ 0027 Remove Element.py
@@ -1,23 +1,30 @@
+# def removeElement(nums, val):
+#     n = len(nums)
+#     if n == 0:
+#         return 0
+#     if n == 1:
+#         if nums[0] == val:
+#             return 0
+#         else:
+#             return 1
+#     i = 0
+#     j = 0
+#     while j <= len(nums)-1:
+#         # print(i, j, nums)
+#         if nums[j] != val:
+#             j += 1
+#             i += 1
+#         else:
+#             nums.remove(nums[j])
+#             j = i
+
+#     return i
+
+# print(removeElement([3,2,2,3], 3))
+
+
 def removeElement(nums, val):
-    n = len(nums)
-    if n == 0:
-        return 0
-    if n == 1:
-        if nums[0] == val:
-            return 0
-        else:
-            return 1
-    i = 0
-    j = 0
-    while j <= len(nums)-1:
-        # print(i, j, nums)
-        if nums[j] != val:
-            j += 1
-            i += 1
-        else:
-            nums.remove(nums[j])
-            j = i
-
-    return i
-
-print(removeElement([3,2,2,3], 3))
+    for i in nums[:]:
+        if i == val:
+            nums.remove(val)
+    return len(nums)
