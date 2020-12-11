d71f667455561efb23f34f103ad5d2eb1e94259c
Yuanyuan Qiu <50243732+YuanyuanQiu@users.noreply.github.com>
2020-12-11 10:02:29 -0600

    Update 0005 Longest Palindromic Substring.py

--- 0005 Longest Palindromic Substring.py
+++ 0005 Longest Palindromic Substring.py
@@ -1,3 +1,4 @@
+# Dynamic Programming
 def longestPalindrome(s):
     n = len(s)
     dp = [[False] * n for _ in range(n)]
@@ -26,5 +27,24 @@ def longestPalindrome(s):
             if dp[i][j] and l + 1 > len(ans):
                 ans = s[i:j+1]
     return ans
+
+
+# Expand Around Center
+def expandAroundCenter(self, s, left, right):
+    while left >= 0 and right < len(s) and s[left] == s[right]:
+        left -= 1
+        right += 1
+    return left + 1, right - 1
+
+def longestPalindrome(self, s: str) -> str:
+    start, end = 0, 0
+    for i in range(len(s)):
+        left1, right1 = self.expandAroundCenter(s, i, i)
+        left2, right2 = self.expandAroundCenter(s, i, i + 1)
+        if right1 - left1 > end - start:
+            start, end = left1, right1
+        if right2 - left2 > end - start:
+            start, end = left2, right2
+    return s[start: end + 1]
 
 print(longestPalindrome("babad"))
