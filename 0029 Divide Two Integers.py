d6f137def40cf6ca91f5ff2f1cc5ed640e9e79a3
Yuanyuan Qiu <50243732+YuanyuanQiu@users.noreply.github.com>
2020-12-30 09:12:17 -0600

    Update 0029 Divide Two Integers.py

--- 0029 Divide Two Integers.py
+++ 0029 Divide Two Integers.py
@@ -1,45 +1,57 @@
-def divide(dividend, divisor):
-    if abs(dividend) < abs(divisor) or dividend ==0:
-        return 0
-   
-    if (dividend > 0 and divisor <0) or (dividend < 0 and divisor > 0):
-        flag = -1
-    else:
-        flag = 1
-    
-    dividend = abs(dividend)
-    divisor = abs(divisor)
+# 方法1：递归
+class Solution:
+    def divide(self, dividend: int, divisor: int) -> int:
+        MIN_INT, MAX_INT = -2147483648, 2147483647  # [−2**31, 2**31−1]
+        flag = 1                                    # 存储正负号，并将分子分母转化为正数
+        if dividend < 0: flag, dividend = -flag, -dividend
+        if divisor < 0: flag, divisor  = -flag, -divisor 
+        
+        def div(dividend, divisor):                 # 例：1023 / 1 = 512 + 256 + 128 + 64 + 32 + 16 + 8 + 4 + 1
+            if dividend < divisor:
+                return 0
+            cur = divisor
+            multiple = 1
+            while cur + cur < dividend:             # 用加法求出保证divisor * multiple <= dividend的最大multiple
+                cur += cur                          # 即cur分别乘以1, 2, 4, 8, 16...2^n，即二进制搜索
+                multiple += multiple
+            return multiple + div(dividend - cur, divisor)
+        res = div(dividend, divisor)
 
-    if divisor == 1:
-        return max(min(dividend * flag, 2147483647), -2147483648)
-    
-    n = len(str(divisor))
-    s = str(dividend)
-    remain = 0
-    end = n
-    digit = int(s[:end])
-    res = ''
-    while end <= len(s):
-        temp = 0
-        # print('s',end, s[:end])
-        # print('digit',digit)
-        while digit >= divisor:
-            temp += 1
-            digit -= divisor
-        res += str(temp)
-        remain = digit
-        # print('remain',remain)
-        # print('result',res)
-        # print('\n')
+        res = res if flag > 0 else -res             # 恢复正负号
         
-        end += 1
-        digit = int(str(remain)+s[end-1:end])
-        
-    
-    return(max(min(int(res)* flag, 2147483647), -2147483648))
+        if res < MIN_INT:                           # 根据是否溢出返回结果
+            return MIN_INT
+        elif MIN_INT <= res <= MAX_INT:
+            return res
+        else:
+            return MAX_INT
                   
-
-print(divide(7,-3))
+# 方法2：迭代
+class Solution:
+    def divide(self, dividend: int, divisor: int) -> int:
+        MIN_INT, MAX_INT = -2147483648, 2147483647  # [−2**31, 2**31−1]
+        flag = 1                                    # 存储正负号，并将分子分母转化为正数
+        if dividend < 0: flag, dividend = -flag, -dividend
+        if divisor < 0: flag, divisor  = -flag, -divisor 
+        
+        res = 0
+        while dividend >= divisor:                  # 例：1023 / 1 = 512 + 256 + 128 + 64 + 32 + 16 + 8 + 4 + 1
+            cur = divisor
+            multiple = 1
+            while cur + cur < dividend:             # 用加法求出保证divisor * multiple <= dividend的最大multiple
+                cur += cur                          # 即cur分别乘以1, 2, 4, 8, 16...2^n，即二进制搜索
+                multiple += multiple
+            dividend -= cur
+            res += multiple
+        
+        res = res if flag > 0 else -res             # 恢复正负号
+        
+        if res < MIN_INT:                           # 根据是否溢出返回结果
+            return MIN_INT
+        elif MIN_INT <= res <= MAX_INT:
+            return res
+        else:
+            return MAX_INT
 
 
 
