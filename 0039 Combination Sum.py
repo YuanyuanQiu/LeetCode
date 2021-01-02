1fdb8082b34fc6a1067a67a344d7919c603698b3
Yuanyuan Qiu <50243732+YuanyuanQiu@users.noreply.github.com>
2021-01-01 19:52:13 -0600

    Update 0039 Combination Sum.py

--- 0039 Combination Sum.py
+++ 0039 Combination Sum.py
@@ -23,36 +23,23 @@
 # 回溯递归剪枝
 class Solution:
     def combinationSum(candidates, target):
-        size = len(candidates)
-        if size == 0:
-            return []
+        candidates = sorted(candidates)
 
-        # 剪枝是为了提速，在本题非必需
-        candidates.sort()
-        # 在遍历的过程中记录路径，它是一个栈
-        path = []
-        res = []
-        # 注意要传入 size ，在 range 中， size 取不到
-        self.__dfs(candidates, 0, size, path, res, target)
-        return res
+        ans = []
 
-    def __dfs(self, candidates, begin, size, path, res, target):
-        # 先写递归终止的情况
-        if target == 0:
-            # Python 中可变对象是引用传递，因此需要将当前 path 里的值拷贝出来
-            # 或者使用 path.copy()
-            res.append(path[:])
-            return
+        def find(s, use, remain):
+            for i in range(s, len(candidates)):
+                c = candidates[i]
+                if c == remain: # 满足条件，加入答案
+                    ans.append(use + [c])
+                if c < remain: # 不足，继续递归
+                    find(i, use + [c], remain - c) # 往后缩小candidate
+                if c > remain: # 超出，退出
+                    return
+        
+        find(0, [], target)
 
-        for index in range(begin, size):
-            residue = target - candidates[index]
-            # “剪枝”操作，不必递归到下一层，并且后面的分支也不必执行
-            if residue < 0:
-                break
-            path.append(candidates[index])
-            # 因为下一层不能比上一层还小，起始索引还从 index 开始
-            self.__dfs(candidates, index, size, path, res, residue)
-            path.pop()
+        return ans
 
 
 if __name__ == '__main__':
