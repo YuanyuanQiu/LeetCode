9cf3b38fc6f6a40384c697e4e156e92b1f789be3
Yuanyuan Qiu <50243732+YuanyuanQiu@users.noreply.github.com>
2021-01-05 22:08:29 -0600

    Create 0079 Word Search.py

---
+++ 0079 Word Search.py
@@ -0,0 +1,37 @@
+# depth-first search
+class Solution:
+    def exist(self, board: List[List[str]], word: str) -> bool:
+        directions = [(0, 1), (0, -1), (1, 0), (-1, 0)]
+        
+        # board[i][j], word[k]
+        def check(i: int, j: int, k: int) -> bool:
+            # 字符不匹配
+            if board[i][j] != word[k]:
+                return False
+            # 已到字符串末尾
+            if k == len(word) - 1:
+                return True
+            
+            visited.add((i, j))
+            result = False
+            for di, dj in directions:
+                newi, newj = i + di, j + dj
+                # 判定不越界
+                if 0 <= newi < len(board) and 0 <= newj < len(board[0]):
+                    # 防止重复
+                    if (newi, newj) not in visited:
+                        # 递归
+                        if check(newi, newj, k + 1):
+                            result = True
+                            break
+            visited.remove((i, j))
+            return result
+
+        h, w = len(board), len(board[0])
+        visited = set()
+        for i in range(h):
+            for j in range(w):
+                if check(i, j, 0):
+                    return True
+        
+        return False
