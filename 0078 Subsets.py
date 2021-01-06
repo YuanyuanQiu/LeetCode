6c45631fa3dd6ee91ac7b35c234c7c40df8ea525
Yuanyuan Qiu <50243732+YuanyuanQiu@users.noreply.github.com>
2021-01-05 19:03:19 -0600

    Update 0078 Subsets.py

--- 0078 Subsets.py
+++ 0078 Subsets.py
@@ -2,17 +2,9 @@ class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
         n = len(nums)
         ans = []
-        # 长度，已使用，剩余
-        def backtrack(lens, used, remain):
-            if len(used) == lens:
-                ans.append(used)
-                return
-            if not remain:
-                return
-            for j in range(len(remain)):
-                backtrack(lens, used + [remain[j]], remain[j+1:])
-        
-        for lens in range(n+1):   
-            backtrack(lens,[],nums)
-        
+        def backtrack(i, used):
+            ans.append(used)
+            for j in range(i,n):
+                backtrack(j+1, used + [nums[j]])    
+        backtrack(0,[])  
         return ans
