0e10c913221444fa17b475006f6dd02adc1e6915
Yuanyuan Qiu <50243732+YuanyuanQiu@users.noreply.github.com>
2021-01-31 19:03:37 -0600

    Update 178.sql

--- 178.sql
+++ 178.sql
@@ -8,13 +8,7 @@ from Scores a
 order by a.Score desc
 
 # Method 2
-select s1.score, s2.Rank
-from 
-    Scores s1
-left join 
-    (select score, dense_rank() over (order by score desc) as 'Rank'
-    from
-        (select distinct score
-        from Scores) t) s2
-on s1.score = s2.score
-order by s1.score desc
+SELECT
+	Score,
+	DENSE_RANK() OVER(ORDER BY Score DESC) AS 'Rank'
+FROM Scores;
