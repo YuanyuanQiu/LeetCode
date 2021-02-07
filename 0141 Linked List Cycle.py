a295ece6d9e30d993af52798b3f2db211c233a6d
Yuanyuan Qiu <50243732+YuanyuanQiu@users.noreply.github.com>
2021-02-07 11:10:55 -0600

    Update 0141 Linked List Cycle.py

--- 0141 Linked List Cycle.py
+++ 0141 Linked List Cycle.py
@@ -1,19 +1,8 @@
 def hasCycle(self, head: ListNode) -> bool:
-    if head is None:
-        return False
-    dic = {}
-    cur = head
-    while True: 
-        # 下一个数已存在于字典中
-        if dic.get(cur.next) is not None:
+    seen = set()
+    while head:
+        if head in seen:
             return True
-
-        # 存入字典
-        dic[cur] = 1
-
-        # 遍历下一个
-        cur = cur.next
-
-        # 遍历到结尾
-        if cur is None:
-            return False
+        seen.add(head)
+        head = head.next
+    return False
