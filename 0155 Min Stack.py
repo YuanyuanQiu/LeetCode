907604577591a165f62be109f43b7b66c1e9be99
Yuanyuan Qiu <50243732+YuanyuanQiu@users.noreply.github.com>
2021-02-07 22:33:53 -0600

    Update 0155 Min Stack.py

--- 0155 Min Stack.py
+++ 0155 Min Stack.py
@@ -1,21 +1,3 @@
-# class MinStack:
-
-#     def __init__(self):
-#         self.ls = []
-
-#     def push(self, x: int) -> None:
-#         self.ls.append(x)
-
-#     def pop(self) -> None:
-#         self.ls.pop()
-
-#     def top(self) -> int:
-#         return self.ls[-1]
-
-#     def getMin(self) -> int:
-#         return min(self.ls)
-
-
 # retrieving the minimum element in constant time.
 class MinStack:
 
