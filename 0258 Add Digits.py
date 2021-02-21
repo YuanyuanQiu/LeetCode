db24898f706a8df972df6a587f1f49d55b2577c2
Yuanyuan Qiu <50243732+YuanyuanQiu@users.noreply.github.com>
2021-02-21 11:04:54 -0600

    Update 0258 Add Digits.py

--- 0258 Add Digits.py
+++ 0258 Add Digits.py
@@ -9,6 +9,16 @@
 #     return num
 
 
+def addDigits(self, num: int) -> int:
+    while num >= 10:
+        sum = 0
+        while num > 0:
+            sum += num % 10
+            num //= 10
+        num = sum
+    return num
+
+
 def addDigits(num):
     if num == 0:
         return 0
