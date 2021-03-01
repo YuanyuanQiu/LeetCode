52b5313a4109a1e1626225f7e5562090eb01efdb
Yuanyuan Qiu <50243732+YuanyuanQiu@users.noreply.github.com>
2021-03-01 14:08:36 -0600

    Update 0441 Arranging Coins.py

--- 0441 Arranging Coins.py
+++ 0441 Arranging Coins.py
@@ -18,6 +18,22 @@ def arrangeCoins(n):
         if i*(i+1)/2>n:
             return i-1
 
+
+def arrangeCoins(self, n: int) -> int:
+    if n <= 1:
+        return n
+    l = 1
+    r = n // 2 + 1
+    while l < r:
+        mid = l + (r - l) // 2 + 1
+        s = mid * (1 + mid) / 2
+        if s > n:
+            r = mid - 1
+        else:
+            l = mid
+    return l
+
+
 print(arrangeCoins(5))
         
             
