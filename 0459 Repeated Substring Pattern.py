5b7848b89225a4bf130211cf0604397207edf40d
Yuanyuan Qiu <50243732+YuanyuanQiu@users.noreply.github.com>
2021-03-01 15:01:29 -0600

    Update 0459 Repeated Substring Pattern.py

--- 0459 Repeated Substring Pattern.py
+++ 0459 Repeated Substring Pattern.py
@@ -12,6 +12,16 @@
 #         n += 1
 #     return False
 
+
+def repeatedSubstringPattern(self, s: str) -> bool:
+    if not s:
+        return True
+    n = len(s)
+    for i in range(n//2):
+        if s == s[:i+1] * (n//(i + 1)):
+            return True
+    return False
+
 '''
 构建双倍链：如果s中包含重复的子字符串，那么说明s中至少包含两个子字符串，
 s+s至少包含4个字串，前后各去掉一位，查找s是否在新构建的字符串中。
