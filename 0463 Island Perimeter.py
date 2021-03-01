3e9998cff8e90b15434cd15a0d44d30390e3844a
Yuanyuan Qiu <50243732+YuanyuanQiu@users.noreply.github.com>
2021-03-01 15:19:06 -0600

    Update 0463 Island Perimeter.py

--- 0463 Island Perimeter.py
+++ 0463 Island Perimeter.py
@@ -1,3 +1,26 @@
+#def islandPerimeter(self, grid: List[List[int]]) -> int:
+#    row = len(grid)
+#    col = len(grid[0])
+#    directions = [(-1,0),(1,0),(0,-1),(0,1)]
+#    res = 0
+#    for i in range(row):
+#        for j in range(col):
+#            if grid[i][j] == 1:
+#                if i == 0:
+#                    res += 1
+#                if i == row - 1:
+#                    res += 1
+#                if j == 0:
+#                    res += 1
+#                if j == col - 1:
+#                    res += 1
+#                for d in directions:
+#                    if i + d[0] >= 0 and i + d[0] < row and j + d[1] >= 0 and j + d[1] < col and grid[i + d[0]][j + d[1]] == 0:
+#                        res += 1
+#    
+#    return res
+
+
 def islandPerimeter(grid) -> int:
     res = 0
     for i in range(len(grid)):
