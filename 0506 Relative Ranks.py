86ff297043ce17112d989cc6313283e4fa3ea05c
Yuanyuan Qiu <50243732+YuanyuanQiu@users.noreply.github.com>
2021-03-02 15:16:22 -0600

    Update 0506 Relative Ranks.py

--- 0506 Relative Ranks.py
+++ 0506 Relative Ranks.py
@@ -1,18 +1,35 @@
-def findRelativeRanks(nums):
-    N = len(nums)
-    if N == 0:
-        return nums
-    
-    sort = sorted(nums, reverse = True)
+#def findRelativeRanks(self, score: List[int]) -> List[str]:
+#    N = len(score)
+#    if N == 0:
+#        return score
+#    
+#    sort = sorted(score, reverse = True)
+#    medals = ["Gold Medal", "Silver Medal", "Bronze Medal"]
+#    for i in range(N):
+#        rank = sort.index(score[i])
+#        if rank <3:
+#            score[i] = medals[rank]
+#        else:
+#            score[i] = str(rank+1)
+#    
+#    return score
+
+
+def findRelativeRanks(self, score: List[int]) -> List[str]:
+    s = sorted(score, reverse=True)
+    dic = {}
+    for i in range(len(s)):
+        dic[s[i]] = i + 1
+    res = []
     medals = ["Gold Medal", "Silver Medal", "Bronze Medal"]
-    for i in range(N):
-        rank = sort.index(nums[i])
-        if rank <3:
-            nums[i] = medals[rank]
+    for j in score:
+        if dic[j] <= 3:
+            res.append(medals[dic[j]-1])
         else:
-            nums[i] = str(rank+1)
-    
-    return nums
+            res.append(str(dic[j]))
+    return res
+
+
 
 print(findRelativeRanks([10,3,8,9,4]))
         
