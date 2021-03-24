5ff3ea288d4b035bc7a83e96a27bbee115a5d24d
Yuanyuan Qiu <50243732+YuanyuanQiu@users.noreply.github.com>
2021-03-23 20:59:07 -0500

    Update 0234 Palindrome Linked List.py

--- 0234 Palindrome Linked List.py
+++ 0234 Palindrome Linked List.py
@@ -1,21 +1,5 @@
 def isPalindrome(head):
     ls = []
-    while head != None:
-        ls.append(head.val)
-        head = head.next
-
-    if len(ls)<=1:
-        return True
-        
-    n = len(ls)//2
-    if ls[:n] == ls[-1*n:][::-1]:
-        return True
-    else:
-        return False
-
-
-def isPalindrome(head):
-    ls = []
     while head:
         ls.append(head.val)
         head = head.next
