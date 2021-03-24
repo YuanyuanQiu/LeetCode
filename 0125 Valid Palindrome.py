0c7c66f721fcca0493c15acd2e7e262c2c807694
Yuanyuan Qiu <50243732+YuanyuanQiu@users.noreply.github.com>
2021-03-23 21:45:18 -0500

    Update 0125 Valid Palindrome.py

--- 0125 Valid Palindrome.py
+++ 0125 Valid Palindrome.py
@@ -7,3 +7,18 @@ def isPalindrome(s):
 
     return new == new[::-1]
 
+def isPalindrome(self, s: str) -> bool:
+    n = len(s)
+    left, right = 0, n - 1
+    
+    while left < right:
+        while left < right and not s[left].isalnum():
+            left += 1
+        while left < right and not s[right].isalnum():
+            right -= 1
+        if left < right:
+            if s[left].lower() != s[right].lower():
+                return False
+            left, right = left + 1, right - 1
+
+    return True
