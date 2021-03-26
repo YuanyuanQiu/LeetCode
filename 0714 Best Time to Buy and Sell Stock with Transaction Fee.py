4e0311939063208d014657557c4242c6d2d81854
Yuanyuan Qiu <50243732+YuanyuanQiu@users.noreply.github.com>
2021-03-26 08:57:04 -0500

    Update 0714 Best Time to Buy and Sell Stock with Transaction Fee.py

--- 0714 Best Time to Buy and Sell Stock with Transaction Fee.py
+++ 0714 Best Time to Buy and Sell Stock with Transaction Fee.py
@@ -1,20 +1,32 @@
-class Solution:
-    def maxProfit(self, prices: List[int], fee: int) -> int:
-        if not prices:
-            return 0
-        n = len(prices)
+def maxProfit(self, prices: List[int], fee: int) -> int:
+    if not prices:
+        return 0
+    n = len(prices)
 
-        # dp[i][j]表示第i天，持有状态为j（0：不持有，1：持有）的最大收益
-        dp = [[0]*2 for _ in range(n)]
+    # dp[i][j]表示第i天，持有状态为j（0：不持有，1：持有）的最大收益
+    dp = [[0]*2 for _ in range(n)]
 
-        # 初始状态
-        dp[0][0] = 0
-        dp[0][1] = -prices[0]
+    # 初始状态
+    dp[0][0] = 0
+    dp[0][1] = -prices[0]
 
-        for i in range(1,n):
-            # i天结束不持有：前一天结束不持有，前一天结束持有本日卖出
-            dp[i][0] = max(dp[i-1][0], dp[i-1][1] + prices[i] - fee)
-            # i天结束持有：前一天结束持有，前一天结束不持有本日买入
-            dp[i][1] = max(dp[i-1][1], dp[i-1][0] - prices[i])
+    for i in range(1,n):
+        # i天结束不持有：前一天结束不持有，前一天结束持有本日卖出
+        dp[i][0] = max(dp[i-1][0], dp[i-1][1] + prices[i] - fee)
+        # i天结束持有：前一天结束持有，前一天结束不持有本日买入
+        dp[i][1] = max(dp[i-1][1], dp[i-1][0] - prices[i])
 
-        return dp[n-1][0]
+    return dp[n-1][0]
+
+
+def maxProfit(self, prices: List[int], fee: int) -> int:
+    n = len(prices)
+    if n <= 1:
+        return 0
+    res = 0
+    buy = -prices[0]
+    sell = 0
+    for i in range(n):
+        buy = max(buy, sell - prices[i])
+        sell = max(sell, buy + prices[i] - fee)
+    return sell
