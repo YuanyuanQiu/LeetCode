106e4bf731e44aef444dc7ce79232f7926721b12
Yuanyuan Qiu <50243732+YuanyuanQiu@users.noreply.github.com>
2021-04-13 17:26:06 -0500

    Update 0022 Generate Parentheses.py

--- 0022 Generate Parentheses.py
+++ 0022 Generate Parentheses.py
@@ -1,3 +1,31 @@
+# backtrack
+def generateParenthesis(self, n: int) -> List[str]:
+    s = '(' * n + ')' * n
+    res = []
+    def backtrack(history, remain):
+        if not remain:
+            res.append(history)
+        for i in range(len(remain)):
+            if i > 0 and remain[i] == remain[i-1]:
+                continue
+            if not history:
+                if remain[i] == ')':
+                    continue
+                else:
+                    backtrack(history + remain[i], remain[:i] + remain[i+1:])
+            elif history[-1] == '(' or remain[i] == '(':
+                backtrack(history + remain[i], remain[:i] + remain[i+1:])
+            else:
+                left = history.count('(')
+                right = history.count(')')
+                if right < left:
+                    backtrack(history + remain[i], remain[:i] + remain[i+1:])
+                else:
+                    continue
+    backtrack('',s)
+    return res
+
+
 # recursion
 # (left)right
 def generateParenthesis(n):
