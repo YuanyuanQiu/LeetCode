f13a16ddeaefd45fc5be8fefd0dd1f7210edfa03
Yuanyuan Qiu <50243732+YuanyuanQiu@users.noreply.github.com>
2021-04-14 10:14:04 -0500

    Update 0094 Binary Tree Inorder Traversal.py

--- 0094 Binary Tree Inorder Traversal.py
+++ 0094 Binary Tree Inorder Traversal.py
@@ -4,15 +4,35 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
-class Solution:
-    def inorderTraversal(self, root: TreeNode) -> List[int]:
-		res = []
-		def dfs(root):
-			if not root:
-				return
-			# 按照 左-打印-右的方式遍历	
-			dfs(root.left)
-			res.append(root.val)
-			dfs(root.right)
-		dfs(root)
-		return res
+
+# Recursion递归
+def inorderTraversal(self, root: TreeNode) -> List[int]:
+    res = []
+    def dfs(root):
+        if not root:
+            return
+        # 按照 左-打印-右的方式遍历	
+        dfs(root.left)
+        res.append(root.val)
+        dfs(root.right)
+    dfs(root)
+    return res
+
+
+# Iteration迭代
+def inorderTraversal(self, root):
+    res = []
+    stack = []
+    while stack or root:
+        # 不断往左子树方向走，每走一次就将当前节点保存到栈中
+        # 这是模拟递归的调用
+        if root:
+            stack.append(root)
+            root = root.left
+        # 当前节点为空，说明左边走到头了，从栈中弹出节点并保存
+        # 然后转向右边节点，继续上面整个过程
+        else:
+            tmp = stack.pop()
+            res.append(tmp.val)
+            root = tmp.right
+    return res
