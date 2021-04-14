00fa0695c0481d5a4b73662d061de7eadf89eaf6
Yuanyuan Qiu <50243732+YuanyuanQiu@users.noreply.github.com>
2021-04-14 14:24:34 -0500

    update

---
+++ 571.sql
@@ -0,0 +1,8 @@
+select avg(number) median
+from
+    (select number,
+        sum(frequency) over(order by number) asc_accumu,
+        sum(frequency) over(order by number desc) desc_accumu
+        from numbers) t1, 
+    (select sum(frequency) total from numbers) t2
+where asc_accumu >= total/2 and desc_accumu >=total/2
