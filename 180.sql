2f4b558140a09f01c8fa51844c59ad2d17483196
Yuanyuan Qiu <50243732+YuanyuanQiu@users.noreply.github.com>
2021-04-17 15:44:47 -0500

    Update 180.sql

--- 180.sql
+++ 180.sql
@@ -1,8 +1,5 @@
 SELECT DISTINCT a.Num AS ConsecutiveNums
 FROM Logs AS a
-INNER JOIN Logs AS b
-ON  a.Id + 1 = b.Id
-INNER JOIN Logs AS c
-ON a.Id + 2 = c.Id
-WHERE a.Num = b.Num
-AND b.Num = c.Num;
+INNER JOIN Logs AS b ON  a.Id + 1 = b.Id
+INNER JOIN Logs AS c ON a.Id + 2 = c.Id
+WHERE a.Num = b.Num AND b.Num = c.Num;
