59a20d33885c9991131eda529574ed3f876da709
Yuanyuan Qiu <50243732+YuanyuanQiu@users.noreply.github.com>
2021-04-19 16:56:46 -0500

    Update 1635.sql

--- 1635.sql
+++ 1635.sql
@@ -1,28 +1,28 @@
 select
     a.month,
-    sum(ifnull(c.c,0)) over (order by a.month) as active_drivers,
+    sum(ifnull(c.total_driver,0)) over (order by a.month) as active_drivers,
     ifnull(b.accepted_rides,0) as accepted_rides
 from 
     (select 1 as month union select 2 union select 3 union select 4 union
     select 5 union select 6 union select 7 union select 8 union
-    select 9 union select 10 union select 11 union select 12)a
+    select 9 union select 10 union select 11 union select 12) a
 left join
     (select month(r.requested_at) as month, count(a.ride_id) as accepted_rides
     from Rides r
     join AcceptedRides a
-    on r.ride_id = a.ride_id and left(r.requested_at,4)='2020'
-    group by 1 # group by the first column
-    )b
+    on r.ride_id = a.ride_id
+    where year(r.requested_at)=2020
+    group by month) b
 on a.month = b.month
 left join
-    (select month, sum(cn) as c
+    (select month, sum(cn) as total_driver
     from
         (
         # after 2020
         (select month(join_date) as month, count(*) as cn
         from Drivers
-        where left(join_date,4)='2020'
-        group by 1)
+        where year(join_date)=2020
+        group by month)
         union all
         # before 2020
         (select '1', count(*)
@@ -31,4 +31,4 @@ left join
         )t2
     group by 1) c
 on a.month = c.month
-order by 1
+order by month
