b085466791e23b3fd0340ec24de358690f945307
Yuanyuan Qiu <50243732+YuanyuanQiu@users.noreply.github.com>
2021-04-19 11:52:04 -0500

    Update 1369.sql

--- 1369.sql
+++ 1369.sql
@@ -1,15 +1,29 @@
-/*select *
-from UserActivity
-group by username
-having count(activity) = 1
+-- select *
+-- from UserActivity
+-- group by username
+-- having count(*) = 1
 
-union all
+-- union all
 
-select username, activity, startDate, endDate
-from
-    (select *, rank() over(partition by username order by startDate desc) as rank1
-    from UserActivity) t
-where rank1 = 2*/
+-- select u1.username, u1.activity, u1.startDate, u1.endDate
+-- from UserActivity u1, UserActivity u2
+-- where u1.username = u2.username and u1.startDate <= u2.startDate
+-- group by u1.username, u1.startDate
+-- having count(u2.startDate) = 2
+
+
+-- select *
+-- from UserActivity
+-- group by username
+-- having count(activity) = 1
+
+-- union all
+
+-- select username, activity, startDate, endDate
+-- from
+--     (select *, rank() over(partition by username order by startDate desc) as rank1
+--     from UserActivity) t
+-- where rank1 = 2
 
 
 SELECT username, activity, startDate, endDate
