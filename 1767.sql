2a6d8a61b0b0c4b501222e03ce594ab136248f5d
Yuanyuan Qiu <50243732+YuanyuanQiu@users.noreply.github.com>
2021-04-20 15:05:10 -0500

    Update 1767.sql

--- 1767.sql
+++ 1767.sql
@@ -1,11 +1,10 @@
-WITH RECURSIVE a AS
-(SELECT 1 AS k
+WITH RECURSIVE f(n) AS
+(SELECT 1
 UNION ALL
-SELECT k+1 FROM a 
-WHERE k<20)
+SELECT n + 1 FROM f WHERE n < 20)
 
-SELECT task_id, k AS subtask_id 
-FROM Tasks, a
-WHERE subtasks_count>=k
-AND (task_id, k) NOT IN (SELECT * FROM Executed)
-ORDER BY task_id, k
+SELECT task_id, n AS subtask_id 
+FROM Tasks, f
+WHERE subtasks_count >= n
+AND (task_id, n) NOT IN (SELECT * FROM Executed)
+ORDER BY task_id, n
