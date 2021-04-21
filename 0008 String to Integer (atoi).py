316ac87778372c43e7998415f9a019d2beedf62e
Yuanyuan Qiu <50243732+YuanyuanQiu@users.noreply.github.com>
2021-04-21 11:58:56 -0500

    Update 0008 String to Integer (atoi).py

--- 0008 String to Integer (atoi).py
+++ 0008 String to Integer (atoi).py
@@ -1,29 +1,27 @@
-# def myAtoi(s):
-#     s = s.strip(' ')
+ def myAtoi(s):
+    s = s.strip()
+    if not s:
+        return 0
+    flag = 1
+    if s[0] == '-':
+        flag = -1
+        s = s[1:]
+    elif s[0] == '+':
+        s = s[1:]
+    for i in range(len(s)):
+        if not s[i].isnumeric():
+            s = s[:i]
+            break
+    if not s:
+        return 0
+    s = flag * int(s)
     
-#     if s == '' or s == '-' or s == '+':
-#         return 0
-    
-#     if s[0].isdigit() == False and s[0] not in '-+':
-#         return 0
-    
-#     if s[0] in '-+':
-#         if s[1].isdigit():
-#             new = s[0]
-#             s = s[1:]
-#         else:
-#             return 0
-#     else:
-#         new = ''
-
-#     for i in s:
-#         if i.isdigit():
-#             new += i
-#         else:
-#             break
-    
-#     res = int(new)
-#     return max(min(res, 2**31 - 1), 2**31 * -1)
+    if s > 2**31 - 1:
+        return 2**31 - 1
+    elif s < -2**31:
+        return -2**31
+    else:
+        return s
 
 
 # lstrip() 方法用于截掉字符串左边的空格或指定字符
