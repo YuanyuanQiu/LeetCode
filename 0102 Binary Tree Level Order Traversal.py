a977305d668cf6de84ca3462c3c5dc609c246ca3
Yuanyuan Qiu <50243732+YuanyuanQiu@users.noreply.github.com>
2021-04-21 12:10:15 -0500

    Update 0102 Binary Tree Level Order Traversal.py

--- 0102 Binary Tree Level Order Traversal.py
+++ 0102 Binary Tree Level Order Traversal.py
@@ -9,21 +9,17 @@ class Solution:
     def levelOrder(self, root: TreeNode) -> List[List[int]]:
         if not root:
             return []
-        #跟结点入queue
-        queue = [root]
         res = []
+        queue = [root]
         while queue:
-            res.append([node.val for node in queue])
-            #存储当前层的孩子节点列表
-            ll = []
-            #对当前层的每个节点遍历
-            for node in queue:
-                #如果左子节点存在，入队列
-                if node.left:
-                    ll.append(node.left)
-                #如果右子节点存在，入队列
-                if node.right:
-                    ll.append(node.right)
-            #后把queue更新成下一层的结点，继续遍历下一层
-            queue = ll
+            cur_level = []
+            next_queue = []
+            for i in queue:
+                cur_level.append(i.val)
+                if i.left:
+                    next_queue.append(i.left)
+                if i.right:
+                    next_queue.append(i.right)
+            res.append(cur_level)
+            queue = next_queue
         return res
