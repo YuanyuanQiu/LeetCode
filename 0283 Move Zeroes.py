1a0c70dc93a426e49de66de7a4ce441e123e08ec
Yuanyuan Qiu <50243732+YuanyuanQiu@users.noreply.github.com>
2021-04-21 14:57:37 -0500

    Update 0283 Move Zeroes.py

--- 0283 Move Zeroes.py
+++ 0283 Move Zeroes.py
@@ -1,13 +1,12 @@
 def moveZeroes(nums):
-    l=r=0
-    while r<len(nums):
-        if nums[l]==0 and nums[r]!=0:
-            nums[l],nums[r]=nums[r],nums[l]
-        if nums[l]!=0:
-            l+=1
-        r+=1
-    return(nums)
-    
-
-print(moveZeroes([0,1,3,0]))
-    
+    """
+    Do not return anything, modify nums in-place instead.
+    """
+    l, r = 0, 0
+    while r < len(nums):
+        if nums[l] == 0 and nums[r] != 0:
+            nums[l], nums[r] = nums[r], nums[l]
+            l += 1
+        elif nums[l] != 0:
+            l += 1
+        r += 1
