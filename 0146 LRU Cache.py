0756d219eb2cd946a704cf895935514449daadbb
Yuanyuan Qiu <50243732+YuanyuanQiu@users.noreply.github.com>
2021-04-20 20:11:20 -0500

    Update 0146 LRU Cache.py

--- 0146 LRU Cache.py
+++ 0146 LRU Cache.py
@@ -2,31 +2,25 @@ class LRUCache:
 
     def __init__(self, capacity: int):
         self.capacity = capacity
-        self.cache = {}
+        self.dic = {}
         self.stack = []
 
     def get(self, key: int) -> int:
-        if key in self.stack:
+        if key in self.dic:
             self.stack.remove(key)
             self.stack.append(key)
-        return self.cache.get(key,-1)
+            return self.dic[key]
+        else:
+            return -1
+
 
     def put(self, key: int, value: int) -> None:
-        if key in self.cache:
-            self.cache[key] = value
+        if key in self.dic:
             self.stack.remove(key)
-            self.stack.append(key)
+            self.dic[key] = value
         else:
             if len(self.stack) == self.capacity:
-                del self.cache[self.stack[0]]
-                self.stack = self.stack[1:]
-            self.cache[key] = value
-            self.stack.append(key)
-
-
-
-
-# Your LRUCache object will be instantiated and called as such:
-# obj = LRUCache(capacity)
-# param_1 = obj.get(key)
-# obj.put(key,value)
+                del self.dic[self.stack[0]]
+                del self.stack[0]
+            self.dic[key] = value
+        self.stack.append(key)
