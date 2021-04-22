c355fd995af7c3b030b6aef53a85c34df84171ab
Yuanyuan Qiu <50243732+YuanyuanQiu@users.noreply.github.com>
2021-04-21 19:21:39 -0500

    Update 0042 Trapping Rain Water.py

--- 0042 Trapping Rain Water.py
+++ 0042 Trapping Rain Water.py
@@ -1,14 +1,16 @@
 def trap(self, height: List[int]) -> int:
-    if not height:
-        return 0
     n = len(height)
-    leftMax = [height[0]] + [0] * (n - 1)
-    for i in range(1, n):
-        leftMax[i] = max(leftMax[i - 1], height[i])
-
-    rightMax = [0] * (n - 1) + [height[n - 1]]
-    for i in range(n - 2, -1, -1):
-        rightMax[i] = max(rightMax[i + 1], height[i])
-
-    ans = sum(min(leftMax[i], rightMax[i]) - height[i] for i in range(n))
+    if n <= 2:
+        return 0
+    left = [0 for _ in range(n)]
+    left[0] = height[0]
+    right = [0 for _ in range(n)]
+    right[-1] = height[-1]
+    for i in range(1,n):
+        left[i] = max(left[i-1], height[i])
+    for j in range(n-2, -1, -1):
+        right[j] = max(right[j+1], height[j])
+    ans = 0
+    for k in range(1,n-1):
+        ans += max(min(left[k], right[k]) - height[k], 0)
     return ans
