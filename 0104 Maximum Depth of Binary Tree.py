51e6364e7b01f23a7405025b42d9863f34d9c6ed
Yuanyuan Qiu <50243732+YuanyuanQiu@users.noreply.github.com>
2021-04-21 19:11:59 -0500

    Update 0104 Maximum Depth of Binary Tree.py

--- 0104 Maximum Depth of Binary Tree.py
+++ 0104 Maximum Depth of Binary Tree.py
@@ -1,5 +1,4 @@
 def maxDepth(self, root: TreeNode) -> int:
     if not root:
         return 0
-    ans = 1
-    return ans + max(self.maxDepth(root.left), self.maxDepth(root.right))
+    return 1 + max(self.maxDepth(root.left), self.maxDepth(root.right))
