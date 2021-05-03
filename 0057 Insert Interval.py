975db85a59f437866f2186fc5b72f1e1c58a2a5a
Yuanyuan Qiu <50243732+YuanyuanQiu@users.noreply.github.com>
2021-05-03 13:11:14 -0500

    Update 0057 Insert Interval.py

--- 0057 Insert Interval.py
+++ 0057 Insert Interval.py
@@ -18,3 +18,27 @@ def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[Lis
         else:
             mreged[-1][1] = max(i[1], mreged[-1][1])
     return mreged
+
+
+def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
+    left, right = newInterval
+    placed = False
+    ans = list()
+    for li, ri in intervals:
+        if li > right:
+            # 在插入区间的右侧且无交集
+            if not placed:
+                ans.append([left, right])
+                placed = True
+            ans.append([li, ri])
+        elif ri < left:
+            # 在插入区间的左侧且无交集
+            ans.append([li, ri])
+        else:
+            # 与插入区间有交集，计算它们的并集
+            left = min(left, li)
+            right = max(right, ri)
+    
+    if not placed:
+        ans.append([left, right])
+    return ans
