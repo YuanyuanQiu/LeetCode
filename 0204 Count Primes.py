6d6e68b6173b6a5f078f22e4f702c28adc6aa10b
Yuanyuan Qiu <50243732+YuanyuanQiu@users.noreply.github.com>
2021-05-07 12:19:26 -0500

    Update 0204 Count Primes.py

--- 0204 Count Primes.py
+++ 0204 Count Primes.py
@@ -1,9 +1,9 @@
 def countPrimes(n):
-    is_prime = [True]*(n+1)
+    is_prime = [True]*n
     ans = 0
-    for num in range(2,n+1):
+    for num in range(2,n):
         if is_prime[num]:
             ans+=1
-            for k in range(1,n//num+1):
+            for k in range(1,(n - 1)//num+1):
                 is_prime[num*k]=False
     return ans
