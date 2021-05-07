e1dccf8cdcd49c29c59225a8e452a88b04524663
Yuanyuan Qiu <50243732+YuanyuanQiu@users.noreply.github.com>
2021-05-07 12:39:48 -0500

    Update 0234 Palindrome Linked List.py

--- 0234 Palindrome Linked List.py
+++ 0234 Palindrome Linked List.py
@@ -5,6 +5,7 @@ def isPalindrome(head):
         head = head.next
     return ls == ls[::-1]
 
+
 # fast and slow pointers
 def isPalindrome(self, head: ListNode) -> bool:
     if not head:
@@ -27,4 +28,25 @@ def isPalindrome(self, head: ListNode) -> bool:
             return False
         head = head.next
         pre = pre.next
+    return True
+
+
+# fast and slow pointers
+def isPalindrome(self, head: ListNode) -> bool:
+    if not head or not head.next:
+        return True
+    fast, slow = head, head
+    half = []
+    while fast and fast.next:
+        half.append(slow.val)
+        slow = slow.next
+        fast = fast.next.next
+    if fast:
+        slow = slow.next
+    while slow:
+        if not half:
+            return False
+        if slow.val != half.pop():
+            return False
+        slow = slow.next
     return True
