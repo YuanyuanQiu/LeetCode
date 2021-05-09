4a9090b92de4fa600f405b95d9a727db5d846be0
Yuanyuan Qiu <50243732+YuanyuanQiu@users.noreply.github.com>
2021-05-08 20:51:00 -0500

    update

--- 1336.sql
+++ 1336.sql
@@ -1,21 +1,20 @@
 with RECURSIVE f(n) as (select 0 as n
-                    union all
-                    select n + 1 as n
-                    from f
-                    where n < (select count(*) from Transactions)),
+                        union
+                        select n + 1 as n
+                        from f
+                        where n < (select count(*) from Transactions)),
 a as (select
-        v.user_id,
-        count(t.transaction_date) as transactions_count
-        from Visits v
-        left join Transactions t
-        on v.user_id = t.user_id and v.visit_date = t.transaction_date
-        group by v.user_id, v.visit_date)
+        count(t.transaction_date) as cnt
+    from Visits v
+    left join Transactions t
+    on v.visit_date = t.transaction_date and v.user_id = t.user_id
+    group by v.user_id, v.visit_date)
 
-
-select f.n as transactions_count, count(a.user_id) as visits_count
+select
+    f.n as transactions_count, count(cnt) as visits_count
 from f
 left join a
-on f.n = a.transactions_count
-where f.n <= (select max(transactions_count) from a)
+on f.n = a.cnt
+where f.n <= (select max(cnt) from a)
 group by f.n
 order by f.n
