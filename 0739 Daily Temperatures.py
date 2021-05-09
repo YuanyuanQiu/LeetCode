86817445c5f263662b13882894f9d7b058c82bc4
Yuanyuan Qiu <50243732+YuanyuanQiu@users.noreply.github.com>
2021-05-09 14:53:01 -0500

    Update 0739 Daily Temperatures.py

--- 0739 Daily Temperatures.py
+++ 0739 Daily Temperatures.py
@@ -1,12 +1,11 @@
-class Solution:
-    def dailyTemperatures(self, T: List[int]) -> List[int]:
-        length = len(T)
-        ans = [0] * length
-        stack = []
-        for i in range(length):
-            temperature = T[i]
-            while stack and temperature > T[stack[-1]]:
-                prev_index = stack.pop()
-                ans[prev_index] = i - prev_index
-            stack.append(i)
-        return ans
+def dailyTemperatures(self, T: List[int]) -> List[int]:
+    length = len(T)
+    ans = [0] * length
+    stack = []
+    for i in range(length):
+        temperature = T[i]
+        while stack and temperature > T[stack[-1]]:
+            prev_index = stack.pop()
+            ans[prev_index] = i - prev_index
+        stack.append(i)
+    return ans
