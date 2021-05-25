81d0db755d3c1b9d0a178736dbf9c6c8c810febe
Yuanyuan Qiu <50243732+YuanyuanQiu@users.noreply.github.com>
2021-05-25 10:31:30 -0500

    update

---
+++ 0098 Validate Binary Search Tree.py
@@ -0,0 +1,16 @@
+def isValidBST(self, root: TreeNode) -> bool:
+    def helper(node, lower = float('-inf'), upper = float('inf')) -> bool:
+        if not node:
+            return True
+        
+        val = node.val
+        if val <= lower or val >= upper:
+            return False
+
+        if not helper(node.right, val, upper):
+            return False
+        if not helper(node.left, lower, val):
+            return False
+        return True
+
+    return helper(root)
