ed53d898498a3c24c6529c79b9d0b102e0834d9a
Yuanyuan Qiu <50243732+YuanyuanQiu@users.noreply.github.com>
2022-05-03 11:51:34 -0400

    Update 0026 Remove Duplicates from Sorted Array.py

--- 0026 Remove Duplicates from Sorted Array.py
+++ 0026 Remove Duplicates from Sorted Array.py
@@ -1,9 +1,11 @@
-def removeDuplicates(nums):
-    f,s = 1,0
-    while f < len(nums):
-        if nums[f] != nums[s]:
-            nums[s+1] = nums[f]
-            s += 1
-        f += 1
-    return len(nums[0:s+1])
-        
+def removeDuplicates(self, nums: List[int]) -> int:
+    n = len(nums)
+    if n <= 1:
+        return n
+    l, r = 1, 1
+    while r < n:
+        if nums[r] != nums[r-1]: #说明nums[r]和之前元素都不同
+            nums[l] = nums[r]
+            l += 1
+        r += 1
+    return l
