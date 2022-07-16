f08c36b3a07c27b796c5bf2a9a147459ee92cb77
Yuanyuan Qiu <50243732+YuanyuanQiu@users.noreply.github.com>
2022-07-16 14:54:51 -0400

    Update 0101 Symmetric Tree.py

--- 0101 Symmetric Tree.py
+++ 0101 Symmetric Tree.py
@@ -1,23 +1,3 @@
-def isSymmetric(self, root: TreeNode) -> bool:
-    if not root:
-        return True
-    elif not root.left and not root.right:
-        return True
-    elif not root.left or not root.right:
-        return False
-    elif root.left.val != root.right.val:
-        return False
-    
-    def mirror(p,q):
-        if not p and not q:
-            return True
-        elif not p or not q:
-            return False
-        return p.val == q.val and mirror(p.left, q.right) and mirror(p.right, q.left)
-
-    return mirror(root.left, root.right)
-
-
 def isSymmetric(self, root):
     if not root:
         return True
