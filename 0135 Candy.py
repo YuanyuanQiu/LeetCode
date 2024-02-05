2c8d8abc56ad7ae338e2ec4ca1ea96631b0ece12
Yuanyuan Qiu <50243732+YuanyuanQiu@users.noreply.github.com>
2024-02-05 15:49:51 -0500

    Update 0135 Candy.py

--- 0135 Candy.py
+++ 0135 Candy.py
@@ -23,3 +23,17 @@ class Solution:
                 # 递减数列的最后一个值一定分配为1
                 pre = 1
         return ret
+    
+class Solution:
+    def candy(self, ratings: List[int]) -> int:
+        n = len(ratings)
+        left, right = [1] * n, [1] * n
+        for i in range(1, n):
+            if ratings[i] > ratings[i - 1]:
+                left[i] = left[i - 1] + 1
+        count = left[-1]
+        for i in range(n - 2, -1, -1):
+            if ratings[i] > ratings[i + 1]:
+                right[i] = right[i + 1] + 1
+            count += max(left[i], right[i])
+        return count
