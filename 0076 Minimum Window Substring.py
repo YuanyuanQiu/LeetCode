7ae5673bceacce06cb612c680de99091c1360880
Yuanyuan Qiu <50243732+YuanyuanQiu@users.noreply.github.com>
2024-07-04 10:11:43 -0400

    Update 0076 Minimum Window Substring.py

--- 0076 Minimum Window Substring.py
+++ 0076 Minimum Window Substring.py
@@ -1,29 +1,14 @@
 def minWindow(self, s: str, t: str) -> str:
-    need = collections.defaultdict(int)
-    for c in t:
-        need[c] += 1
-    needCnt = len(t)
-    i = 0 #记录起始位置
-    res = (0, float('inf'))  #用两个元素，方便之后记录起终点
-    #三步骤：
-    #1. 增加右边界使滑窗包含t
-    for j,c in enumerate(s):
-        if need[c] >0:
-            needCnt -= 1
-        need[c] -= 1 #这行放在外面不可以，看19行 need[c] == 0
-    #2. 收缩左边界直到无法再去掉元素   !注意，处理的是i
-        if needCnt == 0:
-            while True:
-                c = s[i]
-                if need[c] == 0: #表示再去掉就不行了(need>0)
-                    break
-                else:
-                    need[c] += 1
-                    i += 1
-            if j-i < res[1] - res[0]:  #这里是否减一都可以，只要每次都是这样算的就行，反正最后也是输出子串而非长度
-                res = (i,j)
-    #3. i多增加一个位置，准备开始下一次循环(注意这步是在 needCnt == 0里面进行的 )
-            need[s[i]] += 1
-            needCnt += 1    #由于 移动前i这个位置 一定是所需的字母，因此NeedCnt才需要+1
-            i += 1
-    return "" if res[1]>len(s) else s[res[0]: res[1]+1]
+    # ans_left设为-1以排除不存在的极端情况
+    ans_left, ans_right = -1, len(s)
+    left = 0
+    cnt_s = Counter()  # s 子串字母的出现次数
+    cnt_t = Counter(t)  # t 中字母的出现次数
+    for right, c in enumerate(s):  # 移动子串右端点
+        cnt_s[c] += 1  # 右端点字母移入子串
+        while cnt_s >= cnt_t:  # 涵盖
+            if right - left < ans_right - ans_left:  # 找到更短的子串
+                ans_left, ans_right = left, right  # 记录此时的左右端点
+            cnt_s[s[left]] -= 1  # 左端点字母移出子串
+            left += 1  # 移动子串左端点
+    return "" if ans_left < 0 else s[ans_left: ans_right + 1]
