293ab9840c30e18bdf7214e67fe498106c62948d
Yuanyuan Qiu <50243732+YuanyuanQiu@users.noreply.github.com>
2024-08-03 10:20:43 -0400

    Update 0290 Word Pattern.py

--- 0290 Word Pattern.py
+++ 0290 Word Pattern.py
@@ -1,27 +1,16 @@
-def wordPattern(self, pattern, s):
-    """
-    :type pattern: str
-    :type s: str
-    :rtype: bool
-    """
-    ls = s.split()
-    if len(ls)!= len(pattern):
+def wordPattern(self, pattern: str, s: str) -> bool:
+    ls = s.split(' ')
+    m, n = len(pattern), len(ls)
+    if m != n:
         return False
-    dic = dict() # {pattern:s}
-    for i in range(len(pattern)):
+    # create mapping pattern[i]:ls[i]
+    dic = {}
+    for i in range(m):
         if pattern[i] in dic:
             if dic[pattern[i]] != ls[i]:
                 return False
-        elif ls[i] in dic.values():
-            return False
         else:
+            if ls[i] in dic.values():
+                return False
             dic[pattern[i]] = ls[i]
     return True
-
-
-def wordPattern(pattern, str):
-    res=str.split()
-    # pattern.index('a')返回pattern字符串中字符‘a’首次出现的位置
-    return list(map(pattern.index, pattern))==list(map(res.index,res))
-
-print(wordPattern('abba','dog cat cat fish'))
