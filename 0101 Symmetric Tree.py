90840ba95a2e20367797daf9553a610584769fbf
Yuanyuan Qiu <50243732+YuanyuanQiu@users.noreply.github.com>
2024-11-12 19:43:22 -0500

    Update 0101 Symmetric Tree.py

--- 0101 Symmetric Tree.py
+++ 0101 Symmetric Tree.py
@@ -1,10 +1,18 @@
-def isSymmetric(self, root):
-    if not root:
-        return True
-    def help(p,q):
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isInverted(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
         if not p and not q:
             return True
-        if not p or not q:
+        if not p or not q or p.val != q.val:
             return False
-        return p.val == q.val and help(p.left, q.right) and help(p.right, q.left)
-    return help(root.left, root.right)
+        return self.isInverted(p.left, q.right) and self.isInverted(p.right, q.left)
+
+    def isSymmetric(self, root: Optional[TreeNode]) -> bool:
+        if not root:
+            return True
+        return self.isInverted(root.left, root.right)
