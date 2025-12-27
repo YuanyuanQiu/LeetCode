678ea33d8cb8d8ef257d1447b9f92517a17e47a7
Yuanyuan Qiu <50243732+YuanyuanQiu@users.noreply.github.com>
2025-12-27 16:28:05 -0500

    Update 569.sql

--- 569.sql
+++ 569.sql
@@ -1,13 +1,16 @@
-SELECT Id, Company, Salary
-FROM Employee
-WHERE Id in (
-    SELECT e1.Id
-    FROM Employee e1
-    JOIN Employee e2
-    ON e1.Company = e2.Company
-    GROUP BY e1.Id
-    HAVING SUM(CASE WHEN e1.Salary >= e2.Salary THEN 1 ELSE 0 END) >= COUNT(*)/2 
-    AND SUM(CASE WHEN e1.Salary <= e2.Salary THEN 1 ELSE 0 END) >= COUNT(*)/2
-    )
-GROUP BY Company, Salary
-ORDER BY Company
+WITH Ranked AS (
+    SELECT
+        id,
+        company,
+        salary,
+        ROW_NUMBER() OVER (PARTITION BY company ORDER BY salary, id) AS rn,
+        COUNT(*) OVER (PARTITION BY company) AS cnt
+    FROM Employee
+)
+SELECT
+    id,
+    company,
+    salary
+FROM Ranked
+WHERE rn >= cnt/2 AND rn <= cnt/2 + 1
+ORDER BY company, salary;
