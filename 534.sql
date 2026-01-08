6f193f8c986de15d208db88f82e666349747567c
Yuanyuan Qiu <50243732+YuanyuanQiu@users.noreply.github.com>
2026-01-08 14:02:30 -0500

    Update 534.sql

--- 534.sql
+++ 534.sql
@@ -1,6 +1,19 @@
+-- Option 1
+SELECT 
+    t1.player_id, 
+    t1.event_date, 
+    SUM(t2.games_played) AS games_played_so_far
+FROM Activity t1
+JOIN Activity t2 
+    ON t1.player_id = t2.player_id 
+    AND t1.event_date >= t2.event_date
+GROUP BY t1.player_id, t1.event_date
+ORDER BY t1.player_id, t1.event_date;
+
+-- Option 2
 SELECT
-a.player_id, a.event_date, SUM(b.games_played) AS games_played_so_far
-FROM Activity AS a
-INNER JOIN Activity AS b
-ON a.Player_id = b.Player_id AND b.event_date <= a.event_date
-GROUP BY a.player_id, a.event_date
+    player_id,
+    event_date,
+    SUM(games_played) OVER (PARTITION BY player_id ORDER BY event_date) AS games_played_so_far
+FROM Activity
+ORDER BY player_id, event_date;
