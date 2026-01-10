7f6d2ff88f55e38ab50b74240ce4db537f1162c3
Yuanyuan Qiu <50243732+YuanyuanQiu@users.noreply.github.com>
2026-01-10 15:39:13 -0500

    Update 618.sql

--- 618.sql
+++ 618.sql
@@ -1,3 +1,22 @@
+-- Option 1
+WITH Student_rn AS (
+    SELECT
+        name,
+        continent,
+        ROW_NUMBER() OVER (PARTITION BY continent ORDER BY name) AS rn
+    FROM Student
+)
+SELECT
+    s1.name AS America,
+    s2.name AS Asia,
+    s3.name AS Europe
+FROM (SELECT * FROM Student_rn WHERE continent = 'America') s1
+LEFT JOIN (SELECT * FROM Student_rn WHERE continent = 'Asia') s2
+ON s1.rn = s2.rn
+LEFT JOIN (SELECT * FROM Student_rn WHERE continent = 'Europe') s3
+ON s1.rn = s3.rn
+
+-- Option 2
 SELECT 
     MAX(CASE WHEN continent = 'America' THEN name END) AS America,
     MAX(CASE WHEN continent = 'Asia' THEN name END) AS Asia,
