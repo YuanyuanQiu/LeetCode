f2bea8e05a299acc2ef2cb7de6aa5bd4fd60ed42
Yuanyuan Qiu <50243732+YuanyuanQiu@users.noreply.github.com>
2026-01-11 11:06:46 -0500

    Update 0015 3Sum.py

--- 0015 3Sum.py
+++ 0015 3Sum.py
@@ -1,26 +1,33 @@
-def threeSum(nums):
-    n = len(nums)
-    nums.sort()
-    res = []
-    for i in range(n):
-        if nums[i] > 0:
-            return res
-        if i > 0 and nums[i] == nums[i-1]:
-            continue
-        l = i + 1
-        r = n - 1
-        while l < r:
-            val = nums[i] + nums[l] + nums[r]
-            if val == 0:
-                res.append([nums[i], nums[l], nums[r]])
-                while l < r and nums[l] == nums[l+1]:
-                    l += 1
-                while l < r and nums[r] == nums[r-1]:
-                    r -= 1
-                l += 1
-                r -= 1
-            elif val > 0:
-                r -= 1
-            else:
-                l += 1       
-    return res
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        nums.sort()
+        n = len(nums)
+        res = []
+        for i in range(n):
+            if i > 0 and nums[i] == nums[i-1]:
+                continue
+            
+            j = i + 1
+            k = n - 1
+            while j < k:
+                total = nums[i] + nums[j] + nums[k]
+                
+                if total == 0:
+                    res.append([nums[i], nums[j], nums[k]])
+                    
+                    # 1. 跳过左边重复的 (Skip duplicates from left)
+                    while j < k and nums[j] == nums[j+1]:
+                        j += 1
+                    # 2. 跳过右边重复的 (Skip duplicates from right)
+                    while j < k and nums[k] == nums[k-1]:
+                        k -= 1
+                    
+                    # 3. 关键一步：双指针必须同时向内收缩，寻找下一组
+                    j += 1
+                    k -= 1
+                    
+                elif total < 0:
+                    j += 1
+                else:
+                    k -= 1
+        return res
