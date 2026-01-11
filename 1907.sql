8ca7cf6fe63b86c54728b38ea05382410b912eab
Yuanyuan Qiu <50243732+YuanyuanQiu@users.noreply.github.com>
2026-01-10 20:36:23 -0500

    Create 1907.sql

---
+++ 1907.sql
@@ -0,0 +1,32 @@
+-- Option 1
+# Write your MySQL query statement below
+WITH categories AS (
+    SELECT "Low Salary" AS category
+    UNION ALL
+    SELECT "Average Salary" AS category
+    UNION ALL
+    SELECT "High Salary" AS category
+),
+acct_cat AS (
+    SELECT
+        account_id,
+        CASE
+            WHEN income < 20000 THEN "Low Salary"
+            WHEN income > 50000 THEN "High Salary"
+        ELSE "Average Salary" END AS category
+    FROM Accounts
+)
+SELECT
+    c.category,
+    IFNULL(COUNT(DISTINCT account_id), 0) AS accounts_count
+FROM categories c
+LEFT JOIN acct_cat ac
+ON c.category = ac.category
+GROUP BY c.category
+
+-- Option 2
+SELECT 'Low Salary' AS category, SUM(income < 20000) AS accounts_count FROM Accounts
+UNION
+SELECT 'Average Salary', SUM(income BETWEEN 20000 AND 50000) FROM Accounts
+UNION
+SELECT 'High Salary', SUM(income > 50000) FROM Accounts;
